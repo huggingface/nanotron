827b1ef3449c8c7d7bfdc3f937dc451e5989a2c9
Phuc Nguyen <b3f0cus@icloud.com>
2024-01-09 09:56:39 +0000

    add random states's tests

--- src/nanotron/serialize/random.py
+++ src/nanotron/serialize/random.py
@@ -2,8 +2,6 @@ from pathlib import Path
 
 import torch
 
-from nanotron.core import distributed as dist
-from nanotron.core.process_groups import DistributedProcessGroups
 from nanotron.core.random import RandomStates
 from nanotron.distributed import ParallelContext, ParallelMode
 
@@ -29,12 +27,19 @@ def save_random_states(random_states: RandomStates, root_folder: Path, parallel_
     torch.save(random_states, filename)
 
 
-def load_random_states(dpg: DistributedProcessGroups, root_folder: Path):
+def load_random_states(parallel_context: ParallelContext, root_folder: Path):
+    tp_rank = parallel_context.get_local_rank(ParallelMode.TENSOR)
+    pp_rank = parallel_context.get_local_rank(ParallelMode.PIPELINE)
+    dp_rank = parallel_context.get_local_rank(ParallelMode.DATA)
+    tp_world_size = parallel_context.get_world_size(ParallelMode.TENSOR)
+    dp_world_size = parallel_context.get_world_size(ParallelMode.DATA)
+    pp_world_size = parallel_context.get_world_size(ParallelMode.PIPELINE)
+
     # TODO @thomasw21: This basically assumes that we have exactly the same topology as the one we used when saving.
     filename = (
         root_folder
         / "random"
-        / f"tp-{dist.get_rank(dpg.tp_pg)}-of-{dpg.tp_pg.size()}_dp-{dist.get_rank(dpg.dp_pg)}-of-{dpg.dp_pg.size()}_pp-{dist.get_rank(dpg.pp_pg)}-of-{dpg.pp_pg.size()}.pt"
+        / f"tp-{tp_rank}-of-{tp_world_size}_dp-{dp_rank}-of-{dp_world_size}_pp-{pp_rank}-of-{pp_world_size}.pt"
     )
 
     # TODO @thomasw21: That's annothing but this actually uses pickle, we might need to change that for something else
