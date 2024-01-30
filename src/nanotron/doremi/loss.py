3644a27d76e9adae2fb5371aa0bdc42477d70daf
Phuc Nguyen <b3f0cus@icloud.com>
2024-01-30 10:10:03 +0000

    add cross entropy with domain losses

--- src/nanotron/doremi/loss.py
+++ src/nanotron/doremi/loss.py
@@ -1,7 +1,51 @@
+from typing import Dict
+
 import torch
 import torch.distributed as dist
 from nanotron.doremi.doremi_context import DoReMiContext
+from nanotron.doremi.utils import masked_mean
 from nanotron.parallel import ParallelContext
+from nanotron.parallel.tensor_parallel.functional import sharded_cross_entropy
+from torch import nn
+
+
+def compute_per_domain_loss(
+    losses: torch.Tensor, domain_idxs: torch.Tensor, doremi_context: DoReMiContext, parallel_context: ParallelContext
+) -> torch.Tensor:
+    dp_size = dist.get_world_size(parallel_context.dp_pg)
+    dp_pg = parallel_context.dp_pg
+
+    # NOTE: can't do allgather([tensor_list], [tensor]) if a tensor in tensor_list is not contiguous
+    losses_dp = [torch.empty_like(losses, device="cuda").contiguous() for _ in range(dp_size)]
+    dist.all_gather(losses_dp, losses.contiguous(), group=dp_pg)
+    losses_dp = torch.cat(losses_dp, dim=0)
+
+    domain_ids_dp = [torch.empty_like(domain_idxs, device="cuda").contiguous() for _ in range(dp_size)]
+    dist.all_gather(domain_ids_dp, domain_idxs.contiguous(), group=dp_pg)
+    domain_ids_dp = torch.cat(domain_ids_dp, dim=0)
+
+    # NOTE: Calculate total loss per domain
+    N_DOMAINS = doremi_context.num_domains
+    domain_losses = torch.zeros(N_DOMAINS, device="cuda")
+    domain_ids_dp = domain_ids_dp.view(-1)
+
+    assert losses_dp.shape[0] == domain_ids_dp.shape[0]
+    GLOBAL_BATCH_SIZE = losses_dp.shape[0]
+
+    for i in range(GLOBAL_BATCH_SIZE):
+        # NOTE: sum the excess losses of all tokens in the batch
+        # then add it to the domain loss of the corresponding domain
+        # domain_losses[domain_idxs[i]] += excess_losses[i].sum(dim=-1)
+        domain_losses[domain_ids_dp[i]] += losses_dp[i].sum(dim=-1)
+
+    # NOTE: Normalize and smooth domain weights
+    samples_per_domain = torch.bincount(domain_ids_dp, minlength=N_DOMAINS)
+    SEQ_LEN = losses.shape[1]
+    normalized_domain_losses = domain_losses / (samples_per_domain * SEQ_LEN)
+    # NOTE: if the domain loss is zero, then the normalized domain loss is zero
+    normalized_domain_losses[torch.isnan(normalized_domain_losses)] = 0.0
+
+    return domain_losses
 
 
 class DoReMiLossForProxyTraining:
@@ -77,14 +121,40 @@ class DoReMiLossForProxyTraining:
         # return excess_losses, normalized_domain_losses, smooth_domain_weights
         return excess_losses_dp, normalized_domain_losses, smooth_domain_weights
 
-    def _normalize_domain_weights(self, weights: torch.Tensor, smoothing_param: float) -> torch.Tensor:
-        """
-        Renormalize and smooth domain weights.
-        alpha_t = (1 - c) * (alpha_t' / sum(i=1 to k of alpha_t'[i])) + c * u
-        Algorithm 1 DoReMi domain reweighting (Step 2).
-        """
-        # NUM_DOMAINS = weights.shape[0]
-        NUM_DOMAINS = self.doremi_context.num_domains
-        uniform_weights = torch.ones(NUM_DOMAINS, device=weights.device) / NUM_DOMAINS
-        normalized_weight = (1 - smoothing_param) * weights / weights.sum(dim=-1) + (smoothing_param * uniform_weights)
-        return normalized_weight
+    # def _normalize_domain_weights(self, weights: torch.Tensor, smoothing_param: float) -> torch.Tensor:
+    #     """
+    #     Renormalize and smooth domain weights.
+    #     alpha_t = (1 - c) * (alpha_t' / sum(i=1 to k of alpha_t'[i])) + c * u
+    #     Algorithm 1 DoReMi domain reweighting (Step 2).
+    #     """
+    #     # NUM_DOMAINS = weights.shape[0]
+    #     NUM_DOMAINS = self.doremi_context.num_domains
+    #     uniform_weights = torch.ones(NUM_DOMAINS, device=weights.device) / NUM_DOMAINS
+    #     normalized_weight = (1 - smoothing_param) * weights / weights.sum(dim=-1) + (smoothing_param * uniform_weights)
+    #     return normalized_weight
+
+
+class CrossEntropyWithPerDomainLoss(nn.Module):
+    def __init__(self, doremi_context: DoReMiContext, parallel_context: ParallelContext):
+        super().__init__()
+        self.doremi_context = doremi_context
+        self.parallel_context = parallel_context
+
+    def forward(
+        self,
+        sharded_logits: torch.Tensor,  # [seq_length, batch_size, logits]
+        label_ids: torch.Tensor,  # [batch_size, seq_length]
+        label_mask: torch.Tensor,  # [batch_size, seq_length]
+        domain_idxs: torch.Tensor,
+    ) -> Dict[str, torch.Tensor]:
+        # loss = sharded_cross_entropy(
+        #     sharded_logits, label_ids.transpose(0, 1).contiguous(), group=tp_pg, dtype=torch.float
+        # ).transpose(0, 1)
+        per_token_loss = sharded_cross_entropy(
+            sharded_logits, label_ids, group=self.parallel_context.tp_pg, dtype=torch.float
+        )
+        lm_loss = masked_mean(per_token_loss, label_mask, dtype=torch.float)
+        domain_losses = compute_per_domain_loss(
+            per_token_loss, domain_idxs, self.doremi_context, self.parallel_context
+        )
+        return {"loss": lm_loss, "domain_losses": domain_losses}
--- tests/test_doremi_loss.py
+++ tests/test_doremi_loss.py
@@ -6,15 +6,74 @@ from helpers.utils import (
     init_distributed,
 )
 from nanotron.doremi.doremi_context import DoReMiContext
-from nanotron.doremi.loss import DoReMiLossForProxyTraining
+from nanotron.doremi.loss import CrossEntropyWithPerDomainLoss, DoReMiLossForProxyTraining, compute_per_domain_loss
 from nanotron.parallel import ParallelContext
 from nanotron.parallel.tensor_parallel.functional import sharded_cross_entropy
 from nanotron.sanity_checks import assert_tensor_synced_across_pg
 
-# def test_doremi_loss():
 
-#     domain_keys = [f"domain {i}" for i in range(N_DOMAINS)]
-#     domain_weights = F.softmax(torch.ones(N_DOMAINS, requires_grad=False, device="cuda"), dim=-1)
+@pytest.fixture
+def doremi_context():
+    N_DOMAINS = 5
+    domain_keys = [f"domain {i}" for i in range(N_DOMAINS)]
+    domain_weights = F.softmax(torch.ones(N_DOMAINS, requires_grad=False), dim=-1)
+    doremi_context = DoReMiContext(domain_weights, domain_keys, is_proxy=False)
+    return doremi_context
+
+
+def get_partition_logit(logits, parallel_context):
+    tp_size = dist.get_world_size(parallel_context.tp_pg)
+    tp_rank = dist.get_rank(parallel_context.tp_pg)
+    VOCAB_SIZE = logits.shape[-1]
+    per_partition = VOCAB_SIZE // tp_size
+    chunks = torch.split(logits, per_partition, dim=-1)
+    return chunks[tp_rank]
+
+
+@pytest.mark.parametrize("tp", [1, 2])
+def test_computing_per_token_loss(tp: int):
+    BATCH_SIZE = 512
+    SEQ_LEN = 128
+    VOCAB_SIZE = 4
+
+    torch.manual_seed(69)
+
+    logits = torch.randn(BATCH_SIZE, SEQ_LEN, VOCAB_SIZE)
+    targets = torch.randint(0, VOCAB_SIZE, (BATCH_SIZE, SEQ_LEN))
+
+    ref_losses = F.cross_entropy(logits.view(-1, logits.size(2)), targets.view(-1), reduction="none")
+
+    init_distributed(tp=tp, dp=1, pp=1)(_test_computing_per_token_loss)(
+        logits=logits, targets=targets, ref_losses=ref_losses
+    )
+
+
+def _test_computing_per_token_loss(parallel_context: ParallelContext, logits, targets, ref_losses):
+    logits = logits.to("cuda")
+    targets = targets.to("cuda")
+    parallel_logits = get_partition_logit(logits, parallel_context)
+
+    loss = sharded_cross_entropy(parallel_logits, targets, parallel_context.tp_pg)
+
+    assert torch.allclose(loss.cpu().view(-1), ref_losses)
+
+
+@pytest.mark.parametrize("dp", [1, 2])
+def test_doremi_loss(dp: int):
+    GLOBAL_BATCH_SIZE = 512
+    BATCH_SIZE = GLOBAL_BATCH_SIZE // dp
+    SEQ_LEN = 128
+    N_DOMAINS = 5
+    domain_keys = [f"domain {i}" for i in range(N_DOMAINS)]
+    DOMAIN_WEIGHTS = F.softmax(torch.ones(N_DOMAINS, requires_grad=False), dim=-1)
+
+    init_distributed(tp=1, dp=dp, pp=1)(_test_doremi_loss)(
+        global_batch_size=GLOBAL_BATCH_SIZE,
+        batch_size=BATCH_SIZE,
+        seq_len=SEQ_LEN,
+        domain_keys=domain_keys,
+        domain_weights=DOMAIN_WEIGHTS,
+    )
 
 
 def _test_doremi_loss(
@@ -57,54 +116,114 @@ def _test_doremi_loss(
 
 
 @pytest.mark.parametrize("dp", [1, 2])
-def test_doremi_loss(dp: int):
+def test_computing_per_domain_loss(dp: int):
     GLOBAL_BATCH_SIZE = 512
     BATCH_SIZE = GLOBAL_BATCH_SIZE // dp
     SEQ_LEN = 128
     N_DOMAINS = 5
-    domain_keys = [f"domain {i}" for i in range(N_DOMAINS)]
-    DOMAIN_WEIGHTS = F.softmax(torch.ones(N_DOMAINS, requires_grad=False), dim=-1)
 
-    init_distributed(tp=1, dp=dp, pp=1)(_test_doremi_loss)(
-        global_batch_size=GLOBAL_BATCH_SIZE,
+    domain_keys = [f"domain {i}" for i in range(N_DOMAINS)]
+    domain_weights = F.softmax(torch.ones(N_DOMAINS, requires_grad=False), dim=-1)
+
+    init_distributed(tp=1, dp=dp, pp=1)(_test_computing_per_domain_loss)(
         batch_size=BATCH_SIZE,
         seq_len=SEQ_LEN,
         domain_keys=domain_keys,
-        domain_weights=DOMAIN_WEIGHTS,
+        domain_weights=domain_weights,
     )
 
 
-def _test_computing_per_token_loss(parallel_context: ParallelContext, logits, targets, ref_losses):
-    def get_partition(logits, parallel_context):
-        tp_size = dist.get_world_size(parallel_context.tp_pg)
-        tp_rank = dist.get_rank(parallel_context.tp_pg)
-        VOCAB_SIZE = logits.shape[-1]
-        per_partition = VOCAB_SIZE // tp_size
-        chunks = torch.split(logits, per_partition, dim=-1)
-        return chunks[tp_rank]
+def _test_computing_per_domain_loss(
+    parallel_context: ParallelContext, batch_size, seq_len, domain_keys, domain_weights
+):
+    N_DOMAINS = domain_weights.shape[0]
+    domain_weights = domain_weights.to("cuda")
+    losses = torch.randn(batch_size, seq_len, device="cuda")
+    domain_idxs = torch.randint(0, N_DOMAINS, (batch_size,), device="cuda")
 
-    logits = logits.to("cuda")
-    targets = targets.to("cuda")
-    parallel_logits = get_partition(logits, parallel_context)
+    doremi_context = DoReMiContext(domain_weights, domain_keys, is_proxy=False)
 
-    loss = sharded_cross_entropy(parallel_logits, targets, parallel_context.tp_pg)
+    per_domain_loss = compute_per_domain_loss(losses, domain_idxs, doremi_context, parallel_context)
 
-    assert torch.allclose(loss.cpu().view(-1), ref_losses)
+    assert per_domain_loss.shape == (N_DOMAINS,)
+    assert_tensor_synced_across_pg(
+        per_domain_loss, parallel_context.dp_pg, msg=lambda err: f"Per domain loss are not synced across ranks {err}"
+    )
+
+
+# @pytest.mark.parametrize("tp", [1, 2])
+# def test_cross_entropy_with_per_domain_loss(tp: int, doremi_context):
+#     BATCH_SIZE = 512
+#     SEQ_LEN = 128
+#     VOCAB_SIZE = 4
+#     torch.manual_seed(69)
+
+#     logits = torch.randn(BATCH_SIZE, SEQ_LEN, VOCAB_SIZE)
+#     label_ids = torch.randint(0, VOCAB_SIZE, (BATCH_SIZE, SEQ_LEN))
+#     label_mask = torch.ones((BATCH_SIZE, SEQ_LEN), dtype=torch.bool)
+
+#     ref_losses = F.cross_entropy(logits.view(-1, logits.size(2)), label_ids.view(-1), reduction="none")
+
+#     init_distributed(tp=tp, dp=1, pp=1)(_test_cross_entropy_with_per_domain_loss)(
+#         logits=logits, label_ids=label_ids, label_mask=label_mask, ref_losses=ref_losses, doremi_context=doremi_context, batch_size=BATCH_SIZE
+#     )
+
+
+# def _test_cross_entropy_with_per_domain_loss(parallel_context: ParallelContext, logits, label_ids, label_mask, ref_losses, batch_size, doremi_context):
+#     N_DOMAINS = doremi_context.num_domains
+
+#     logits = logits.to("cuda")
+#     label_ids = label_ids.to("cuda")
+#     label_mask = label_mask.to("cuda")
+#     parallel_logits = get_partition_logit(logits, parallel_context)
+#     domain_idxs = torch.randint(0, N_DOMAINS, (batch_size,), device="cuda")
+
+#     loss_func = CrossEntropyWithPerDomainLoss(doremi_context, parallel_context)
+#     outputs = loss_func(parallel_logits, label_ids, label_mask, domain_idxs)
+
+#     assert torch.allclose(outputs["loss"].cpu().view(-1), ref_losses)
+#     assert 1 == 1
 
 
 @pytest.mark.parametrize("tp", [1, 2])
-def test_computing_per_token_loss(tp: int):
+def test_cross_entropy_with_per_domain_loss(tp: int, doremi_context):
     BATCH_SIZE = 512
     SEQ_LEN = 128
     VOCAB_SIZE = 4
+    N_DOMAINS = doremi_context.num_domains
 
     torch.manual_seed(69)
 
     logits = torch.randn(BATCH_SIZE, SEQ_LEN, VOCAB_SIZE)
-    targets = torch.randint(0, VOCAB_SIZE, (BATCH_SIZE, SEQ_LEN))
+    label_ids = torch.randint(0, VOCAB_SIZE, (BATCH_SIZE, SEQ_LEN))
+    label_mask = torch.ones((BATCH_SIZE, SEQ_LEN), dtype=torch.bool)
+    domain_idxs = torch.randint(0, N_DOMAINS, (BATCH_SIZE,))
 
-    ref_losses = F.cross_entropy(logits.view(-1, logits.size(2)), targets.view(-1), reduction="none")
+    ref_losses = F.cross_entropy(logits.view(-1, logits.size(2)), label_ids.view(-1))
 
-    init_distributed(tp=tp, dp=1, pp=1)(_test_computing_per_token_loss)(
-        logits=logits, targets=targets, ref_losses=ref_losses
+    init_distributed(tp=tp, dp=1, pp=1)(_test_cross_entropy_with_per_domain_loss)(
+        logits=logits,
+        label_ids=label_ids,
+        label_mask=label_mask,
+        domain_idxs=domain_idxs,
+        ref_losses=ref_losses,
+        doremi_context=doremi_context,
     )
+
+
+def _test_cross_entropy_with_per_domain_loss(
+    parallel_context: ParallelContext, logits, label_ids, label_mask, domain_idxs, ref_losses, doremi_context
+):
+    logits = logits.to("cuda")
+    label_ids = label_ids.to("cuda")
+    label_mask = label_mask.to("cuda")
+    domain_idxs = domain_idxs.to("cuda")
+
+    parallel_logits = get_partition_logit(logits, parallel_context)
+
+    # loss = sharded_cross_entropy(parallel_logits, label_ids, parallel_context.tp_pg)
+    loss_func = CrossEntropyWithPerDomainLoss(doremi_context, parallel_context)
+    outputs = loss_func(parallel_logits, label_ids, label_mask, domain_idxs)
+
+    assert torch.allclose(outputs["loss"].cpu().view(-1), ref_losses)
+    assert outputs["domain_losses"].shape == (doremi_context.num_domains,)
