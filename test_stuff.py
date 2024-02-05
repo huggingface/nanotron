b70b75832e8d899e1fa233968cc93881bc1a0d48
Phuc Nguyen <b3f0cus@icloud.com>
2024-02-05 06:04:51 +0000

    new config

--- test_stuff.py
+++ test_stuff.py
@@ -1,5 +1,5 @@
 import torch
 
-domain_weights = torch.load("/fsx/phuc/checkpoints/doremi/big-run-01/proxy-280m-llama/doremi_domain_weights_4000.pt")
+domain_weights = torch.load("/fsx/phuc/checkpoints/doremi/big-run-02/proxy-280m-llama/doremi_domain_weights_89000.pt")
 
 assert 1 == 1
