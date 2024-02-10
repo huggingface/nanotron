8a98cfcda56b824bf7aad55f738fcfb0b5a79d2d
Phuc Nguyen <b3f0cus@icloud.com>
2024-02-10 07:17:23 +0000

    fix expectation

--- tests/test_tensor_parallel.py
+++ tests/test_tensor_parallel.py
@@ -1,4 +1,5 @@
 import os
+from contextlib import nullcontext as does_not_raise
 from typing import Any
 
 import pytest
@@ -148,11 +149,12 @@ def _test_column_linear(
 @pytest.mark.parametrize("tp,dp,pp", [pytest.param(i, 1, 1) for i in range(1, min(4, available_gpus()) + 1)])
 @pytest.mark.parametrize("tp_mode", list(TensorParallelLinearMode))
 @pytest.mark.parametrize("async_communication", [False, True])
-def test_row_linear(
-    tp: int, dp: int, pp: int, tp_mode: TensorParallelLinearMode, async_communication: bool, expectation: Any
-):
+def test_row_linear(tp: int, dp: int, pp: int, tp_mode: TensorParallelLinearMode, async_communication: bool):
     if tp_mode is TensorParallelLinearMode.ALL_REDUCE and async_communication:
         pytest.skip("ALL_REDUCE mode does not support async communication")
+
+    # NOTE: we expect all the current configurations don't raise any exceptions
+    expectation = does_not_raise()
     init_distributed(tp=tp, dp=dp, pp=pp)(_test_row_linear)(
         tp_mode=tp_mode, async_communication=async_communication, expectation=expectation
     )
