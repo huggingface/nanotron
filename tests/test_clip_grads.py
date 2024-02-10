e3c3d1132af8bffed8133fae73948edf9b031909
Phuc Nguyen <b3f0cus@icloud.com>
2024-02-10 07:39:28 +0000

    fix checking total_norm should be a scalar

--- tests/test_clip_grads.py
+++ tests/test_clip_grads.py
@@ -422,7 +422,7 @@ def _test_clip_grads_tied_weights(parallel_context: ParallelContext, norm_type: 
         norm_type=norm_type,
     )
     ref_total_norm = torch.nn.utils.clip_grad_norm_([ref_weight, ref_bias], max_norm=1.0, norm_type=norm_type)
-    assert len(total_norm.shape) == 0, f"total_norm should be a scalar. Got {total_norm}"
+    assert total_norm.dim() == 0, f"total_norm should be a scalar. Got {total_norm}"
 
     # Check that the gradients have changed
     assert not torch.allclose(old_grad, weight.grad), "Gradients should have changed after clipping"
