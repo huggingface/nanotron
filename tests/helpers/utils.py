2a4e735cb61135e7543f77912e681d6017688515
Phuc Nguyen <b3f0cus@icloud.com>
2024-02-10 10:35:53 +0000

    run tests in parallel

--- tests/helpers/utils.py
+++ tests/helpers/utils.py
@@ -190,7 +190,7 @@ def get_all_3d_configurations(gpus: int) -> List[Tuple[int, int, int]]:
     return result
 
 
-def rerun_if_address_is_in_use(max_try: int = 100):
+def rerun_if_address_is_in_use(max_try: int = 150):
     """
     This function reruns a wrapped function if "address already in use" occurs
     in testing spawned with torch.multiprocessing
--- tests/test_distributed.py
+++ tests/test_distributed.py
@@ -33,6 +33,6 @@ def _test_init_parallel_context(parallel_context: ParallelContext):
         for all_3d_configs in get_all_3d_configurations(gpus)
     ],
 )
-@rerun_if_address_is_in_use(max_try=150)
+@rerun_if_address_is_in_use()
 def test_init_parallel_context(tp: int, dp: int, pp: int):
     init_distributed(tp=tp, dp=dp, pp=pp)(_test_init_parallel_context)()
