wip fix loss scaling for fp8

src/nanotron/fp8/loss_scaler.py
@@ -2,6 +2,7 @@ import torch
 from torch.optim import Optimizer
 
 from nanotron.fp8.constants import LS_INITIAL_SCALING_FACTOR, LS_INITIAL_SCALING_VALUE, LS_INTERVAL
+from nanotron.fp8.tensor import FP8Tensor, FP16Tensor
 
 
 class LossScaler:
@@ -53,7 +54,10 @@ class LossScaler:
                 for p in group["params"]:
                     if p.grad is not None:
                         # TODO(xrsrke): do inplace operation
-                        p.grad = p.grad / self.scaling_value
+                        if isinstance(p.grad, FP8Tensor) or isinstance(p.grad, FP16Tensor):
+                            p.grad.fp8_meta._inverse_scale = p.grad.fp8_meta.inverse_scale * (1 / self.scaling_value)
+                        else:
+                            p.grad = p.grad / self.scaling_value
 
             optim.step(*args, **kwargs)
 

src/nanotron/fp8/meta.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Union
+from typing import Union, Optional
 
 import torch
 import transformer_engine as te  # noqa
@@ -18,6 +18,7 @@ class FP8Meta:
 
     # TODO(xrsrke): change to Literal[torch.int8, torch.uint8]
     dtype: torch.dtype
+    _inverse_scale: Optional[torch.Tensor] = None
 
     @property
     def te_dtype(self) -> tex.DType:
@@ -34,7 +35,9 @@ class FP8Meta:
 
     @property
     def inverse_scale(self) -> torch.Tensor:
-        return 1 / self.scale
+        # TODO(xrsrke): this is a hacky way, remove the _inverse_scale
+        return 1 / self.scale if self._inverse_scale is None else self._inverse_scale
+
 
     def __repr__(self) -> str:
         return f"FP8Meta(amax={self.amax}, scale={self.scale}, inverse_scale={self.inverse_scale}, dtype={self.dtype})"

tests/fp8/test_fp8_optim.py
@@ -97,6 +97,8 @@ def test_fp8_optim_master_weights_fp16_and_fp8():
 @pytest.mark.parametrize("eps", [1e-8, 1e-3])
 @pytest.mark.parametrize("weight_decay", [0, 0.5])
 def test_fp8adam_optimizer_states(learning_rate, betas, eps, weight_decay):
+    # NOTE: this one works
+    # learning_rate, betas, eps, weight_decay = ((0.001, (0.9, 0.999), 1e-08, 0))
     input = torch.randn(16, 16, device="cuda")
     linear = nn.Linear(16, 16, device="cuda")
     fp8_linear = convert_linear_to_fp8(deepcopy(linear), accum_qtype=DTypes.KFLOAT16)
@@ -104,21 +106,15 @@ def test_fp8adam_optimizer_states(learning_rate, betas, eps, weight_decay):
     optim = Adam(linear.parameters(), learning_rate, betas, eps, weight_decay)
     fp8_optim = FP8Adam(fp8_linear.parameters(), learning_rate, betas, eps, weight_decay)
 
-    # for _ in range(4):
-    #     optim.zero_grad()
-    #     fp8_optim.zero_grad()
+    for _ in range(1):
+        optim.zero_grad()
+        fp8_optim.zero_grad()
 
-    #     linear(input).sum().backward()
-    #     fp8_linear(input).sum().backward()
+        linear(input).sum().backward()
+        fp8_linear(input).sum().backward()
 
-    #     optim.step()
-    #     fp8_optim.step()
-
-    linear(input).sum().backward()
-    set_fake_fp8_grads(fp8_linear, linear)
-
-    optim.step()
-    fp8_optim.step()
+        optim.step()
+        fp8_optim.step()
 
     for (_, ref_state), (_, fp8_state) in zip(optim.state.items(), fp8_optim.state.items()):
         fp32_exp_avg = convert_tensor_from_fp8(fp8_state["exp_avg"], fp8_state["exp_avg"].fp8_meta, torch.float32)
@@ -152,36 +148,9 @@ def test_fp8adam_optimizer_state_dtypes():
         assert fp8_state["exp_avg_sq"].dtype == torch.float16
 
 
-# @pytest.mark.parametrize("n_steps", [1, 5])
-@pytest.mark.parametrize(
-    "learning_rate",
-    [
-        1e-3,
-        # 1
-    ],
-)
-@pytest.mark.parametrize(
-    "betas",
-    [
-        (0.9, 0.999),
-        # (0.9, 0.99)
-    ],
-)
-@pytest.mark.parametrize(
-    "eps",
-    [
-        1e-8,
-        # 1e-3
-    ],
-)
-@pytest.mark.parametrize(
-    "weight_decay",
-    [
-        1e-3,
-        # 0
-    ],
-)
-def test_fp8adam_step(learning_rate, betas, eps, weight_decay):
+def test_fp8adam_step():
+    learning_rate, betas, eps, weight_decay = (0.001, (0.9, 0.999), 1e-08, 0)
+
     linear = nn.Linear(16, 16, device="cuda")
     fp8_linear = convert_linear_to_fp8(deepcopy(linear), accum_qtype=DTypes.KFLOAT16)
 
@@ -223,12 +192,25 @@ def test_fp8adam_zero_grad():
     assert [p.grad is None for p in fp8_linear.parameters()]
 
 
-@pytest.mark.parametrize("scaling_value", [torch.tensor(2, dtype=torch.float32).pow(15), torch.tensor(2, dtype=torch.float32).pow(2)])
+@pytest.mark.parametrize("scaling_value", [
+    torch.tensor(2, dtype=torch.float32).pow(1),
+    torch.tensor(2, dtype=torch.float32).pow(2),
+    torch.tensor(2, dtype=torch.float32).pow(3),
+    torch.tensor(2, dtype=torch.float32).pow(4),
+    torch.tensor(2, dtype=torch.float32).pow(5),
+    torch.tensor(2, dtype=torch.float32).pow(6),
+    torch.tensor(2, dtype=torch.float32).pow(7),
+    torch.tensor(2, dtype=torch.float32).pow(8),
+    torch.tensor(2, dtype=torch.float32).pow(9),
+    torch.tensor(2, dtype=torch.float32).pow(10),
+    torch.tensor(2, dtype=torch.float32).pow(11),
+    torch.tensor(2, dtype=torch.float32).pow(12),
+    torch.tensor(2, dtype=torch.float32).pow(13),
+    torch.tensor(2, dtype=torch.float32).pow(14),
+    torch.tensor(2, dtype=torch.float32).pow(15),
+])
 def test_fp8adam_step_with_loss_scaling(scaling_value):
-    LR = 1e-3
-    BETAS = (0.9, 0.999)
-    EPS = 1e-8
-    WEIGHT_DECAY = 1e-3
+    LR, BETAS, EPS, WEIGHT_DECAY = ((0.001, (0.9, 0.999), 1e-08, 0))
     
     ref_linear = nn.Linear(16, 16, device="cuda")
     fp8_linear = convert_linear_to_fp8(deepcopy(ref_linear), accum_qtype=DTypes.KFLOAT16)

tests/fp8/test_tensor.py
@@ -10,12 +10,14 @@ from nanotron.fp8.meta import FP8Meta
 from nanotron.fp8.tensor import FP8Tensor, FP16Tensor, convert_tensor_from_fp8, convert_tensor_from_fp16
 
 
-@pytest.mark.parametrize("dtype", [DTypes.FP8E4M3, DTypes.FP8E5M2])
-def test_fp8_meta_of_a_fp8_tensor(dtype):
+@pytest.mark.parametrize(
+    "tensor_cls, dtype", [(FP8Tensor, DTypes.FP8E4M3), (FP8Tensor, DTypes.FP8E5M2), (FP16Tensor, DTypes.KFLOAT16)]
+)
+def test_fp8_and_fp16_metadata(tensor_cls, dtype):
     tensor = torch.randn((4, 4), dtype=torch.float32, device="cuda")
     ref_tensor = deepcopy(tensor)
 
-    fp8_tensor = FP8Tensor(tensor, dtype=dtype)
+    fp8_tensor = tensor_cls(tensor, dtype=dtype)
 
     # TODO(xrsrke): remove the fixed 1 factor
     # it couples with the current implementation of FP8Meta