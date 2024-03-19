fix FP8 gradients' correctness in loss scaling

src/nanotron/fp8/linear.py
@@ -6,26 +6,32 @@ import transformer_engine as te  # noqa
 from torch import nn
 
 from nanotron.fp8.constants import FP8LM_RECIPE, QTYPE_TO_DTYPE
+from nanotron.fp8.dtypes import DTypes
 from nanotron.fp8.kernel import fp8_matmul_kernel
 from nanotron.fp8.parameter import FP8Parameter
 from nanotron.fp8.tensor import FP8Tensor
-from nanotron.fp8.dtypes import DTypes
-import pydevd
 
 
 class FP8Linear(nn.Linear):
     # TODO(xrsrke): qtype isn't the data types of the weight and bias
     # but the accumulation precision dtype
     # chanege it to accum_dtype
-    def __init__(self, in_features: int, out_features: int, bias: bool = True, device: Optional[torch.device] = None, accum_qtype: DTypes = DTypes.KFLOAT32):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = True,
+        device: Optional[torch.device] = None,
+        accum_qtype: DTypes = DTypes.KFLOAT32,
+    ):
         """
         Args:
             qtype (DTypes, optional): This is accumulation precision dtype
         """
-        
+
         assert device != torch.device("cpu"), "FP8Linear only supports CUDA tensors"
         assert accum_qtype in DTypes
-        
+
         super().__init__(in_features, out_features, bias, device, QTYPE_TO_DTYPE[accum_qtype])
         # TODO(xrsrke): don't fixed dtype, take it from the FP8 recipe
         # DTypes.FP8E4M3
@@ -54,7 +60,9 @@ class FP8Linear(nn.Linear):
 class _FP8Matmul(torch.autograd.Function):
     @staticmethod
     @torch.no_grad()
-    def forward(ctx, input: Union[FP8Tensor, torch.Tensor], weight: FP8Tensor, phony: torch.Tensor, accum_qtype: DTypes) -> torch.Tensor:
+    def forward(
+        ctx, input: Union[FP8Tensor, torch.Tensor], weight: FP8Tensor, phony: torch.Tensor, accum_qtype: DTypes
+    ) -> torch.Tensor:
         if type(input) == torch.Tensor:
             input = FP8Tensor(input, dtype=FP8LM_RECIPE.linear.input.dtype)
 
@@ -64,9 +72,12 @@ class _FP8Matmul(torch.autograd.Function):
 
         # NOTE: pass FP8Tensor instead of FP8Parameter
         output = fp8_matmul_kernel(
-            mat_a=weight.data, transpose_a=True, mat_b=input, transpose_b=False, use_split_accumulator=False,
-            accum_qtype=accum_qtype
-            
+            mat_a=weight.data,
+            transpose_a=True,
+            mat_b=input,
+            transpose_b=False,
+            use_split_accumulator=False,
+            accum_qtype=accum_qtype,
         )
 
         return output, phony
@@ -87,21 +98,28 @@ class _FP8Matmul(torch.autograd.Function):
 
         # TODO(xrsrke): remove fixed grad_output
         if type(grad_output) == torch.Tensor:
-            grad_output = torch.ones_like(grad_output)
+            # grad_output = torch.ones_like(grad_output)
             grad_output = grad_output.contiguous()
-            # DTypes.FP8E5M2
             grad_output = FP8Tensor(grad_output, dtype=FP8LM_RECIPE.linear.output_grad.dtype)
-        
+
         # TODO(xrsrke): extract use_split_accumulator from FP8Recipe
         grad_input = fp8_matmul_kernel(
-            mat_a=grad_output, transpose_a=True, mat_b=weight, transpose_b=True, use_split_accumulator=True,
-            accum_qtype=accum_qtype
+            mat_a=grad_output,
+            transpose_a=True,
+            mat_b=weight,
+            transpose_b=True,
+            use_split_accumulator=True,
+            accum_qtype=accum_qtype,
         )
         grad_weight = fp8_matmul_kernel(
-            mat_a=input, transpose_a=False, mat_b=grad_output, transpose_b=False, use_split_accumulator=True,
-            accum_qtype=accum_qtype
+            mat_a=input,
+            transpose_a=False,
+            mat_b=grad_output,
+            transpose_b=False,
+            use_split_accumulator=True,
+            accum_qtype=accum_qtype,
         )
-        
+
         assert grad_input.dtype == QTYPE_TO_DTYPE[accum_qtype]
         assert grad_weight.dtype == QTYPE_TO_DTYPE[accum_qtype]
         # TODO(xrsrke): maintain a persistence metadata across training

src/nanotron/fp8/loss_scaler.py
@@ -1,7 +1,11 @@
+from typing import List, Union
+
 import torch
+from torch import nn
 from torch.optim import Optimizer
 
 from nanotron.fp8.constants import LS_INITIAL_SCALING_FACTOR, LS_INITIAL_SCALING_VALUE, LS_INTERVAL
+from nanotron.fp8.parameter import FP8Parameter
 from nanotron.fp8.tensor import FP8Tensor, FP16Tensor
 
 
@@ -16,6 +20,8 @@ class LossScaler:
     ):
         # NOTE: because the precision of these scaling factors
         # affect the precision of the gradients
+        assert isinstance(scaling_value, torch.Tensor)
+        assert isinstance(scaling_factor, torch.Tensor)
         assert scaling_value.dtype == torch.float32
         assert scaling_factor.dtype == torch.float32
         assert interval > 0
@@ -52,15 +58,27 @@ class LossScaler:
             # NOTE: unscale gradients
             for group in optim.param_groups:
                 for p in group["params"]:
-                    if p.grad is not None:
-                        # TODO(xrsrke): do inplace operation
-                        if isinstance(p.grad, FP8Tensor) or isinstance(p.grad, FP16Tensor):
-                            # p.grad.fp8_meta._inverse_scale = p.grad.fp8_meta.inverse_scale * (1 / self.scaling_value)
-                            p.grad.mul_(2/self.scaling_value)
-                        else:
-                            p.grad = p.grad / self.scaling_value
+                    # if p.requires_grad and p.grad is not None:
+                    #     # TODO(xrsrke): do inplace operation
+                    #     if isinstance(p.grad, FP8Tensor) or isinstance(p.grad, FP16Tensor):
+                    #         p.grad.mul_(1 / self.scaling_value)
+                    #     else:
+                    #         p.grad = p.grad / self.scaling_value
+                    self._unscale_param_(p)
 
             optim.step(*args, **kwargs)
+
+    def unscale_(self, params: List[Union[nn.Parameter, FP8Parameter]]):
+        """Unscale the gradients of parameters in place."""
+        for p in params:
+            self._unscale_param_(p)
+
+    def _unscale_param_(self, p: Union[nn.Parameter, FP8Parameter]):
+        if p.requires_grad and p.grad is not None:
+            if isinstance(p.grad, FP8Tensor) or isinstance(p.grad, FP16Tensor):
+                p.grad.mul_(1 / self.scaling_value)
+            else:
+                p.grad.div_(self.scaling_value)
 
     def update(self):
         # TODO(xrsrke): remove this
@@ -76,6 +94,7 @@ def is_overflow(tensor: torch.Tensor) -> bool:
         return True
     else:
         return False
+
 
 def is_underflow():
     pass

tests/fp8/test_fp8_optim.py
@@ -6,6 +6,7 @@ import torch
 from nanotron.fp8.constants import FP8LM_RECIPE
 from nanotron.fp8.dtypes import DTypes
 from nanotron.fp8.linear import FP8Linear
+from nanotron.fp8.loss_scaler import LossScaler
 from nanotron.fp8.optim import Adam as REFAdam
 from nanotron.fp8.optim import (
     FP8Adam,
@@ -20,7 +21,6 @@ from nanotron.fp8.tensor import (
 from torch import nn
 from torch.optim import Adam
 from utils import convert_linear_to_fp8, convert_to_fp8_module
-from nanotron.fp8.loss_scaler import LossScaler
 
 
 def set_fake_fp8_grads(linear: FP8Linear, ref_linear: Optional[nn.Linear] = None) -> FP8Linear:
@@ -29,14 +29,14 @@ def set_fake_fp8_grads(linear: FP8Linear, ref_linear: Optional[nn.Linear] = None
         if ref_linear is None
         else ref_linear.weight.grad
     )
-    weight_grad = FP8Tensor(weight_grad, dtype=FP8LM_RECIPE.linear.weight_grad.dtype)
+    weight_grad = FP8Tensor(deepcopy(weight_grad), dtype=FP8LM_RECIPE.linear.weight_grad.dtype)
 
     bias_grad = (
         torch.randn_like(linear.bias.data, device="cuda", dtype=torch.float32)
         if ref_linear is None
         else ref_linear.bias.grad
     )
-    bias_grad = FP16Tensor(bias_grad, dtype=DTypes.KFLOAT16)
+    bias_grad = FP16Tensor(deepcopy(bias_grad), dtype=DTypes.KFLOAT16)
 
     linear.weight.grad = weight_grad
     linear.bias.grad = bias_grad
@@ -58,7 +58,7 @@ def test_fp8_optim_default_initiation():
 #     linear = nn.Linear(16, 16, device="cuda")
 #     fp8_linear = convert_linear_to_fp8(deepcopy(linear), accum_qtype=DTypes.KFLOAT16)
 #     fp8_optim = FP8Adam(fp8_linear.parameters())
-    
+
 #     assert 1 == 1
 
 
@@ -98,7 +98,6 @@ def test_fp8_optim_master_weights_fp16_and_fp8():
 @pytest.mark.parametrize("weight_decay", [0, 0.5])
 def test_fp8adam_optimizer_states(learning_rate, betas, eps, weight_decay):
     # NOTE: this one works
-    # learning_rate, betas, eps, weight_decay = ((0.001, (0.9, 0.999), 1e-08, 0))
     input = torch.randn(16, 16, device="cuda")
     linear = nn.Linear(16, 16, device="cuda")
     fp8_linear = convert_linear_to_fp8(deepcopy(linear), accum_qtype=DTypes.KFLOAT16)
@@ -130,12 +129,12 @@ def test_fp8adam_optimizer_states(learning_rate, betas, eps, weight_decay):
 
 
 def test_fp8adam_optimizer_state_dtypes():
-    # input = torch.randn(16, 16, device="cuda")
+    input = torch.randn(16, 16, device="cuda")
     linear = nn.Linear(16, 16, device="cuda")
     fp8_linear = convert_linear_to_fp8(deepcopy(linear), accum_qtype=DTypes.KFLOAT16)
 
     fp8_optim = FP8Adam(fp8_linear.parameters())
-    set_fake_fp8_grads(fp8_linear)
+    fp8_linear(input).sum().backward()
     fp8_optim.step()
 
     for _, fp8_state in fp8_optim.state.items():
@@ -149,14 +148,16 @@ def test_fp8adam_optimizer_state_dtypes():
 
 
 def test_fp8adam_step():
-    learning_rate, betas, eps, weight_decay = (0.001, (0.9, 0.999), 1e-08, 0)
+    LR, BETAS, EPS, WEIGHT_DECAY = (0.001, (0.9, 0.999), 1e-08, 0)
 
+    input = torch.randn(16, 16, device="cuda")
     linear = nn.Linear(16, 16, device="cuda")
     fp8_linear = convert_linear_to_fp8(deepcopy(linear), accum_qtype=DTypes.KFLOAT16)
 
-    optim = Adam(linear.parameters(), learning_rate, betas, eps, weight_decay)
-    fp8_optim = FP8Adam(fp8_linear.parameters(), learning_rate, betas, eps, weight_decay)
-    input = torch.randn(16, 16, device="cuda")
+    # optim = Adam(linear.parameters(), LR, BETAS, EPS, WEIGHT_DECAY)
+    # fp8_optim = FP8Adam(fp8_linear.parameters(), LR, BETAS, EPS, WEIGHT_DECAY)
+    optim = Adam(linear.parameters(), weight_decay=WEIGHT_DECAY)
+    fp8_optim = FP8Adam(fp8_linear.parameters(), weight_decay=WEIGHT_DECAY)
 
     for _ in range(1):
         linear(input).sum().backward()
@@ -192,50 +193,54 @@ def test_fp8adam_zero_grad():
     assert [p.grad is None for p in fp8_linear.parameters()]
 
 
-@pytest.mark.parametrize("scaling_value", [
-    torch.tensor(2, dtype=torch.float32).pow(1),
-    torch.tensor(2, dtype=torch.float32).pow(2),
-    torch.tensor(2, dtype=torch.float32).pow(3),
-    torch.tensor(2, dtype=torch.float32).pow(4),
-    torch.tensor(2, dtype=torch.float32).pow(5),
-    torch.tensor(2, dtype=torch.float32).pow(6),
-    torch.tensor(2, dtype=torch.float32).pow(7),
-    torch.tensor(2, dtype=torch.float32).pow(8),
-    torch.tensor(2, dtype=torch.float32).pow(9),
-    torch.tensor(2, dtype=torch.float32).pow(10),
-    torch.tensor(2, dtype=torch.float32).pow(11),
-    torch.tensor(2, dtype=torch.float32).pow(12),
-    torch.tensor(2, dtype=torch.float32).pow(13),
-    torch.tensor(2, dtype=torch.float32).pow(14),
-    torch.tensor(2, dtype=torch.float32).pow(15),
-])
+@pytest.mark.parametrize(
+    "scaling_value",
+    [
+        torch.tensor(2, dtype=torch.float32).pow(1),
+        torch.tensor(2, dtype=torch.float32).pow(2),
+        torch.tensor(2, dtype=torch.float32).pow(3),
+        torch.tensor(2, dtype=torch.float32).pow(4),
+        torch.tensor(2, dtype=torch.float32).pow(5),
+        torch.tensor(2, dtype=torch.float32).pow(6),
+        torch.tensor(2, dtype=torch.float32).pow(7),
+        torch.tensor(2, dtype=torch.float32).pow(8),
+        torch.tensor(2, dtype=torch.float32).pow(9),
+        torch.tensor(2, dtype=torch.float32).pow(10),
+        torch.tensor(2, dtype=torch.float32).pow(11),
+        torch.tensor(2, dtype=torch.float32).pow(12),
+        torch.tensor(2, dtype=torch.float32).pow(13),
+        torch.tensor(2, dtype=torch.float32).pow(14),
+        torch.tensor(2, dtype=torch.float32).pow(15),
+    ],
+)
 def test_fp8adam_step_with_loss_scaling(scaling_value):
-    LR, BETAS, EPS, WEIGHT_DECAY = ((0.001, (0.9, 0.999), 1e-08, 0))
-    
+    LR, BETAS, EPS, WEIGHT_DECAY = (0.001, (0.9, 0.999), 1e-08, 0)
+    # LR, BETAS, EPS, WEIGHT_DECAY = ((0.001, (0.9, 0.999), 1e-08, 0))
+
+    input = torch.randn(16, 16, device="cuda")
     ref_linear = nn.Linear(16, 16, device="cuda")
     fp8_linear = convert_linear_to_fp8(deepcopy(ref_linear), accum_qtype=DTypes.KFLOAT16)
     loss_scaler = LossScaler(scaling_value)
 
     ref_optim = Adam(ref_linear.parameters(), LR, BETAS, EPS, WEIGHT_DECAY)
     fp8_optim = FP8Adam(fp8_linear.parameters(), LR, BETAS, EPS, WEIGHT_DECAY)
-    input = torch.randn(16, 16, device="cuda")
 
     for _ in range(1):
         ref_loss = ref_linear(input).sum()
         ref_loss.backward()
         ref_optim.step()
-        # optim.zero_grad()
-        
+        ref_optim.zero_grad()
+
         loss = fp8_linear(input).sum()
         loss = loss.to(torch.float32)
         scaled_loss = loss_scaler.scale(loss)
-        
+
         # TODO(xrsrke): remove these sanity checks after debugging
         assert not torch.allclose(scaled_loss, ref_loss)
-        
+
         scaled_loss.backward()
         loss_scaler.step(fp8_optim)
-        # fp8_optim.zero_grad()
+        fp8_optim.zero_grad()
 
     # NOTE: since optimizer update depends on the gradients
     # and in this test we only want to check whether fp8 optim step is correct
@@ -244,6 +249,7 @@ def test_fp8adam_step_with_loss_scaling(scaling_value):
     weight_fp32 = convert_tensor_from_fp8(fp8_linear.weight.data, fp8_linear.weight.data.fp8_meta, torch.float32)
     # NOTE: this specific threshold is based on the FP8-LM implementation
     # the paper shows that it don't hurt convergence
+    # source: https://github.com/Azure/MS-AMP/blob/0a2cd721fa68ee725e3b2fb132df02ddb8069d62/tests/optim/test_adamw.py#L85
     torch.testing.assert_allclose(weight_fp32, ref_linear.weight, rtol=0, atol=3e-4)
     torch.testing.assert_allclose(fp8_linear.bias, ref_linear.bias, rtol=0, atol=3e-4)
 

tests/fp8/test_loss_scaler.py
@@ -2,9 +2,12 @@ from copy import deepcopy
 
 import pytest
 import torch
+from nanotron.fp8.dtypes import DTypes
 from nanotron.fp8.loss_scaler import LossScaler, is_overflow
+from nanotron.fp8.tensor import convert_tensor_from_fp8
 from torch import nn
 from torch.optim import Adam
+from utils import convert_linear_to_fp8
 
 
 def test_loss_scaler_attributes():
@@ -19,14 +22,24 @@ def test_loss_scaler_attributes():
     assert loss_scaler.interval == interval
 
 
+# def test_loss_scaler_auto_cast_loss_to_fp32():
+#     input = torch.randn(4, 4)
+#     linear = nn.Linear(4, 4)
+#     loss_scaler = LossScaler()
+
+#     scaled_loss = loss_scaler.scale(linear(input).sum())
+
+#     assert scaled_loss.dtype == torch.float32
+
+
 def test_gradients_correctness():
     input = torch.randn(4, 4)
     linear = nn.Linear(4, 4)
     ref_linear = deepcopy(linear)
-    loss = linear(input).sum()
-
     loss_scaler = LossScaler()
-    scaled_loss = loss_scaler.scale(ref_linear(input).sum())
+
+    scaled_loss = loss_scaler.scale(linear(input).sum())
+    loss = ref_linear(input).sum()
 
     assert not torch.allclose(scaled_loss, loss)
 
@@ -35,6 +48,57 @@ def test_gradients_correctness():
 
     assert not torch.allclose(linear.weight.grad, ref_linear.weight.grad)
     assert not torch.allclose(linear.bias.grad, ref_linear.bias.grad)
+
+    assert torch.equal(linear.weight.grad, ref_linear.weight.grad * loss_scaler.scaling_value)
+    assert torch.equal(linear.bias.grad, ref_linear.bias.grad * loss_scaler.scaling_value)
+
+
+def test_unscale_scaled_gradients():
+    input = torch.randn(4, 4)
+    linear = nn.Linear(4, 4)
+    ref_linear = deepcopy(linear)
+    loss_scaler = LossScaler()
+
+    scaled_loss = loss_scaler.scale(linear(input).sum())
+    loss = ref_linear(input).sum()
+
+    loss.backward()
+    scaled_loss.backward()
+    loss_scaler.unscale_(linear.parameters())
+
+    assert torch.equal(linear.weight.grad, ref_linear.weight.grad)
+    assert torch.equal(linear.bias.grad, ref_linear.bias.grad)
+
+
+def test_fp8_gradients_correctness():
+    SCALING_VALUE = torch.tensor(10.0, dtype=torch.float32)
+    input = torch.randn(16, 16, device="cuda")
+    fp32_linear = nn.Linear(16, 16, device="cuda")
+
+    loss_scaler = LossScaler(scaling_value=SCALING_VALUE)
+    linear = convert_linear_to_fp8(deepcopy(fp32_linear), DTypes.KFLOAT16)
+    ref_linear = convert_linear_to_fp8(deepcopy(fp32_linear), DTypes.KFLOAT16)
+
+    ref_loss = ref_linear(input).sum()
+    loss = linear(input).sum()
+    scaled_loss = loss_scaler.scale(loss.to(torch.float32))
+
+    assert torch.equal(scaled_loss, ref_loss.to(torch.float32) * SCALING_VALUE)
+
+    ref_loss.backward()
+    scaled_loss.backward()
+
+    assert not torch.equal(linear.weight.grad, ref_linear.weight.grad)
+    assert not torch.equal(linear.bias.grad, ref_linear.bias.grad)
+
+    loss_scaler.unscale_(linear.parameters())
+    weight_grad = convert_tensor_from_fp8(linear.weight.grad, linear.weight.grad.fp8_meta, torch.float32)
+    ref_weight_grad = convert_tensor_from_fp8(ref_linear.weight.grad, ref_linear.weight.grad.fp8_meta, torch.float32)
+
+    # NOTE: because the scaling value is 10, so if the scaled gradients aren't scaled back
+    # properly, the absolute difference would be around 10, we use 1 for sanity check
+    assert torch.allclose(weight_grad, ref_weight_grad, atol=1.0)
+    assert torch.equal(linear.bias.grad, ref_linear.bias.grad)
 
 
 def test_loss_scaler_step():
@@ -137,7 +201,7 @@ def test_overflow(tensor, expected_output):
         tensor[0, 0] = torch.tensor(float("inf"))
 
     output = is_overflow(tensor)
-    
+
     assert isinstance(output, bool)
     assert output is expected_output
 