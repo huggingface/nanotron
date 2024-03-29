f294469f7a5f0248c0308e670afab67c59ea09bb
Phuc Nguyen <b3f0cus@icloud.com>
2024-03-29 06:24:53 +0000

    add loss scaling in sanity script

--- tests/fp8/reference_convergence/run_nanotron_fp8.py
+++ tests/fp8/reference_convergence/run_nanotron_fp8.py
@@ -4,7 +4,8 @@ import msamp
 import torch
 from nanotron.fp8.dtypes import DTypes
 from nanotron.fp8.optim import FP8Adam
-from nanotron.fp8.utils import convert_linear_to_fp8
+from nanotron.fp8.utils import convert_linear_to_fp8, convert_to_fp8_module
+from nanotron.fp8.loss_scaler import LossScaler
 from torch import nn
 from torch.optim import Adam
 
@@ -21,13 +22,18 @@ def get_time_name():
 if __name__ == "__main__":
     BATCH_SIZE = 16
     HIDDEN_SIZE = 16
-    N_STEPS = 20000
+    N_STEPS = 1000
     LR = 1e-3
+    N_LAYERS = 1
 
-    ref_linear = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE, device="cuda")
+    # ref_linear = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE, device="cuda")
+    ref_linear = nn.Sequential(*[nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE).to('cuda') for _ in range(N_LAYERS)])
     fp8_linear = deepcopy(ref_linear)
-
+    fp8_linear_with_scaler = deepcopy(ref_linear)
     msamp_linear = deepcopy(ref_linear)
+    
+    ref_optim = Adam(ref_linear.parameters(), lr=LR)
+
     msamp_optim = Adam(msamp_linear.parameters(), lr=LR)
     msamp_linear, msamp_optim = msamp.initialize(msamp_linear, msamp_optim, opt_level="O2")
 
@@ -37,29 +43,50 @@ if __name__ == "__main__":
         msamp_linear_with_scaler, msamp_optim_with_scaler, opt_level="O2"
     )
 
-    ref_optim = Adam(ref_linear.parameters(), lr=LR)
     # msamp_optim = LBAdam(msamp_linear.parameters(), lr=LR)
 
-    fp8_linear = convert_linear_to_fp8(fp8_linear, accum_qtype=DTypes.KFLOAT16)
+    fp8_linear = convert_to_fp8_module(fp8_linear, accum_qtype=DTypes.KFLOAT16)
     fp8_optim = FP8Adam(fp8_linear.parameters(), lr=LR)
+    
+    fp8_linear_with_scaler = convert_to_fp8_module(fp8_linear_with_scaler, accum_qtype=DTypes.KFLOAT16)
+    fp8_optim_with_scaler = FP8Adam(fp8_linear_with_scaler.parameters(), lr=LR)
 
     # inputs = torch.randn(HIDDEN_SIZE, HIDDEN_SIZE, device="cuda", requires_grad=False)
 
     wandb.init(
         project="fp8_for_nanotron",
         name=f"{get_time_name()}.sanity_fp8",
+        config={
+            "batch_size": BATCH_SIZE,
+            "hidden_size": HIDDEN_SIZE,
+            "n_steps": N_STEPS,
+            "lr": LR,
+            "n_layers": 1
+        }
     )
 
     msamp_scaler = torch.cuda.amp.GradScaler()
+    fp8_scaler = LossScaler()
 
     # def loss_func(x):
     #     return F.sigmoid(x) * 5 + 0.5
     loss_func = nn.CrossEntropyLoss()
 
-    for _ in range(N_STEPS):
+    # batch_inputs = []
+    # batch_targets = []
+    # inputs = torch.randn(BATCH_SIZE, HIDDEN_SIZE).to("cuda")
+    # targets = torch.randint(0, HIDDEN_SIZE, (BATCH_SIZE,)).to("cuda")
+    # for _ in range(N_STEPS):
+    #     batch_inputs.append(inputs.clone())
+    #     batch_targets.append(targets.clone())
+
+
+    for step in range(N_STEPS):
+        # inputs = batch_inputs[step]
+        # targets = batch_targets[step]
         inputs = torch.randn(BATCH_SIZE, HIDDEN_SIZE).to("cuda")
         targets = torch.randint(0, HIDDEN_SIZE, (BATCH_SIZE,)).to("cuda")
-
+        
         ref_output = ref_linear(inputs)
         loss = loss_func(ref_output, targets)
         loss.backward()
@@ -71,6 +98,12 @@ if __name__ == "__main__":
         fp8_loss.backward()
         fp8_optim.step()
         fp8_optim.zero_grad()
+        
+        fp8_output_with_scaler = fp8_linear_with_scaler(inputs)
+        fp8_loss_with_scaler = loss_func(fp8_output_with_scaler, targets)
+        fp8_scaler.scale(fp8_loss_with_scaler)
+        fp8_scaler.step(fp8_optim_with_scaler)
+        fp8_scaler.update()
 
         msamp_output = msamp_linear(inputs)
         msamp_loss = loss_func(msamp_output, targets)
@@ -89,6 +122,7 @@ if __name__ == "__main__":
             {
                 "fp32_loss": loss.item(),
                 "fp8_loss": fp8_loss.item(),
+                "fp8_loss_with_scaler": fp8_loss_with_scaler.item(),
                 "msamp_o2_loss": msamp_loss.item(),
                 "msamp_o2_loss_with_scaler": msamp_loss_with_scaler.item(),
             }
