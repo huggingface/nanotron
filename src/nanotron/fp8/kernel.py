fix overflow

src/nanotron/fp8/kernel.py
@@ -67,11 +67,33 @@ def fp8_matmul_kernel(
     mat_a = tex.fp8_transpose(mat_a, mat_a_fp8_meta.te_dtype) if transpose_a is False else mat_a
     mat_b = tex.fp8_transpose(mat_b, mat_b_fp8_meta.te_dtype) if transpose_b is True else mat_b
 
+    # if transpose_a is True:
+    #     mat_a = mat_a
+    # elif transpose_a is False:
+    #     mat_a = tex.fp8_transpose(mat_a, mat_a_fp8_meta.te_dtype)
+
+    # if transpose_b is False:
+    #     mat_b = mat_b
+    # elif transpose_b is True:
+    #     mat_b = tex.fp8_transpose(mat_b, mat_b_fp8_meta.te_dtype)
+
+    # if transpose_a is False:
+    #     mat_a = mat_a
+    # elif transpose_a is True:
+    #     mat_a = tex.fp8_transpose(mat_a, mat_a_fp8_meta.te_dtype)
+
+    # if transpose_b is True:
+    #     mat_b = mat_b
+    # elif transpose_b is False:
+    #     mat_b = tex.fp8_transpose(mat_b, mat_b_fp8_meta.te_dtype)
+
     # output = torch.empty(mat_a.T.shape[0], mat_b.shape[-1], device=device, dtype=out_torch_dtype)
     if is_backward is False:
         output = torch.empty(mat_b.shape[0], mat_a.shape[0], device=device, dtype=out_torch_dtype)
+        # output = torch.empty(mat_b.shape[-1], mat_a.shape[-1], device=device, dtype=out_torch_dtype)
     else:
         output = torch.empty(mat_b.shape[0], mat_a.shape[0], device=device, dtype=out_torch_dtype)
+        # output = torch.empty(mat_a.shape[-1], mat_b.shape[-1], device=device, dtype=out_torch_dtype)
 
     tex.te_gemm(
         mat_a,
@@ -246,21 +268,32 @@ def _fp8_matmul_kernel_2(
     # orig_mat_a_shape = deepcopy(mat_a.shape)
     # orig_mat_b_shape = deepcopy(mat_b.shape)
 
-    mat_a_new_shape = mat_a.T.shape if transpose_a is True and TE_CONFIG_TRANSPOSE_A is True else mat_a.T.shape
-    mat_b_new_shape = mat_b.T.shape if transpose_b is True and TE_CONFIG_TRANSPOSE_B is True else mat_b.T.shape
+    # mat_a_new_shape = mat_a.T.shape if transpose_a is True and TE_CONFIG_TRANSPOSE_A is True else mat_a.T.shape
+    # mat_b_new_shape = mat_b.T.shape if transpose_b is True and TE_CONFIG_TRANSPOSE_B is True else mat_b.T.shape
 
-    mat_a = tex.fp8_transpose(mat_a, mat_a_fp8_meta.te_dtype) if transpose_a is False else mat_a
-    mat_b = tex.fp8_transpose(mat_b, mat_b_fp8_meta.te_dtype) if transpose_b is True else mat_b
+    # mat_a = tex.fp8_transpose(mat_a, mat_a_fp8_meta.te_dtype) if transpose_a is False else mat_a
+    # mat_b = tex.fp8_transpose(mat_b, mat_b_fp8_meta.te_dtype) if transpose_b is True else mat_b
+
+    if transpose_a is True:
+        mat_a_transposed = mat_a
+    elif transpose_a is False:
+        mat_a_transposed = tex.fp8_transpose(mat_a, mat_a_fp8_meta.te_dtype)
+
+    if transpose_b is False:
+        mat_b_transposed = mat_b
+    elif transpose_b is True:
+        mat_b_transposed = tex.fp8_transpose(mat_b, mat_b_fp8_meta.te_dtype)
 
     # output = torch.empty(mat_a.T.shape[0], mat_b.shape[-1], device=device, dtype=out_torch_dtype)
-    output = torch.empty(mat_a_new_shape[0], mat_b_new_shape[-1], device=device, dtype=out_torch_dtype)
+    # output = torch.empty(mat_a_new_shape[0], mat_b_new_shape[-1], device=device, dtype=out_torch_dtype)
+    output = torch.empty(mat_a_transposed.shape[-1], mat_b_transposed.shape[-1], device=device, dtype=out_torch_dtype)
 
     tex.te_gemm(
-        mat_a,
+        mat_a_transposed,
         mat_a_fp8_meta.inverse_scale,
         mat_a_fp8_meta.te_dtype,
         TE_CONFIG_TRANSPOSE_A,
-        mat_b,
+        mat_b_transposed,
         mat_b_fp8_meta.inverse_scale,
         mat_b_fp8_meta.te_dtype,
         TE_CONFIG_TRANSPOSE_B,

src/nanotron/fp8/linear.py
@@ -3,11 +3,12 @@ from typing import Optional, Tuple, Union
 import torch
 import torch.nn.functional as F
 import transformer_engine as te  # noqa
+import transformer_engine_extensions as tex
 from torch import nn
 
 from nanotron.fp8.constants import FP8LM_RECIPE, QTYPE_TO_DTYPE
 from nanotron.fp8.dtypes import DTypes
-from nanotron.fp8.kernel import _fp8_matmul_kernel_2, fp8_matmul_kernel
+from nanotron.fp8.kernel import fp8_matmul_kernel
 from nanotron.fp8.parameter import FP8Parameter
 from nanotron.fp8.tensor import FP8Tensor
 
@@ -101,18 +102,19 @@ class _FP8Matmul(torch.autograd.Function):
             transpose_a=True,
             mat_b=input,
             transpose_b=False,
-            # NOTE: input @ weight.T
-            # mat_a=input,
-            # transpose_a=False,
-            # mat_b=weight.data,
-            # transpose_b=True,
-            # mat_a=weight.data,
-            # transpose_a=False,
-            # mat_b=input,
-            # transpose_b=False,
             use_split_accumulator=False,
             accum_qtype=accum_qtype,
         )
+
+        # output = _fp8_matmul_kernel_2(
+        #     # NOTE: that works
+        #     mat_a=input,
+        #     transpose_a=False,
+        #     mat_b=weight.data,
+        #     transpose_b=True,
+        #     use_split_accumulator=False,
+        #     accum_qtype=accum_qtype,
+        # )
 
         return output, phony
 
@@ -153,22 +155,50 @@ class _FP8Matmul(torch.autograd.Function):
         #     is_backward=True
         # )
 
-        grad_input = _fp8_matmul_kernel_2(
-            mat_a=grad_output,
+        grad_output_transposed = tex.fp8_transpose(grad_output, grad_output.fp8_meta.te_dtype)
+        grad_output_transposed.fp8_meta = grad_output.fp8_meta
+        weight_transposed = tex.fp8_transpose(weight, weight.fp8_meta.te_dtype)
+        weight_transposed.fp8_meta = weight.fp8_meta
+
+        grad_input = fp8_matmul_kernel(
+            mat_a=weight_transposed,
             transpose_a=True,
-            mat_b=weight,
-            transpose_b=True,
-            use_split_accumulator=True,
-            accum_qtype=accum_qtype,
-        )
-        grad_weight = _fp8_matmul_kernel_2(
-            mat_a=input,
-            transpose_a=False,
             mat_b=grad_output,
             transpose_b=False,
             use_split_accumulator=True,
             accum_qtype=accum_qtype,
+            # is_backward=True
         )
+
+        input_tranposed = tex.fp8_transpose(input, input.fp8_meta.te_dtype)
+        input_tranposed.fp8_meta = input.fp8_meta
+
+        grad_weight = fp8_matmul_kernel(
+            mat_a=input_tranposed,
+            transpose_a=True,
+            mat_b=grad_output_transposed,
+            transpose_b=False,
+            use_split_accumulator=True,
+            accum_qtype=accum_qtype,
+            # is_backward=True
+        )
+
+        # grad_input = _fp8_matmul_kernel_2(
+        #     mat_a=grad_output,
+        #     transpose_a=True,
+        #     mat_b=weight,
+        #     transpose_b=True,
+        #     use_split_accumulator=True,
+        #     accum_qtype=accum_qtype,
+        # )
+        # grad_weight = _fp8_matmul_kernel_2(
+        #     mat_a=input,
+        #     transpose_a=False,
+        #     mat_b=grad_output,
+        #     transpose_b=False,
+        #     use_split_accumulator=True,
+        #     accum_qtype=accum_qtype,
+        # )
 
         # grad_input = _fp8_matmul_kernel(
         #     mat_a=grad_output,

tests/fp8/reference_convergence/run_msamp_bloom.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-import math
 from copy import deepcopy
 
 import msamp
@@ -12,8 +11,9 @@ from nanotron.fp8.dtypes import DTypes
 from nanotron.fp8.loss_scaler import LossScaler
 from nanotron.fp8.optim import FP8Adam
 from nanotron.fp8.utils import convert_to_fp8_module
+from timm.models.layers import trunc_normal_
 from torch.utils.data import DataLoader
-from transformers import BloomConfig, BloomForCausalLM, BloomTokenizerFast
+from transformers import BloomConfig, BloomTokenizerFast
 
 import wandb
 
@@ -36,21 +36,56 @@ def l1_norm_diff(loss, ref_loss):
     return (loss - ref_loss).abs().mean()
 
 
+from dataclasses import dataclass
+
+
+@dataclass
+class ModelOutput:
+    logits: torch.Tensor
+
+
+class ToyModel(nn.Module):
+    def __init__(self, config: BloomConfig):
+        super().__init__()
+        self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size)
+        # self.net = nn.Sequential(
+        #     nn.Linear(config.hidden_size, config.vocab_size),
+        # )
+        self.linear = nn.Linear(config.hidden_size, config.vocab_size)
+
+        # self.linear.weight.data.normal_(mean=0.0, std=0.02)
+        # self.word_embeddings.weight.data.normal_(mean=0.0, std=0.02)
+        trunc_normal_(self.linear.weight.data, std=0.02)
+        trunc_normal_(self.word_embeddings.weight.data, std=0.02)
+
+    def forward(self, input_ids, attention_mask):
+        x = self.word_embeddings(input_ids)
+        # return self.net(x)
+        return ModelOutput(logits=self.linear(x))
+
+
 def main():
     """The main function."""
 
     SEED = 42
-    LR = 6e-4
+    # LR = 6e-4
+    LR = 0.01
     SEQ_LEN = 128
     BATCH_SIZE = 16
     N_EPOCHS = 1
 
     # MODEL_NAME = "gpt2"
     MODEL_NAME = "bigscience/bloom"
-    HIDDEN_SIZE = 768
-    N_LAYERS = 12
-    N_HEADS = 12
-    INITIALIZER_RANGE = math.sqrt(1 / HIDDEN_SIZE)
+    HIDDEN_SIZE = 16
+    N_LAYERS = 1
+    N_HEADS = 2
+
+    # HIDDEN_SIZE = 16
+    # N_LAYERS = 1
+    # N_HEADS = 2
+
+    # INITIALIZER_RANGE = math.sqrt(1 / HIDDEN_SIZE)
+    INITIALIZER_RANGE = 0.02
 
     # NOTE: CohereForAI/aya_dataset: 200k examples
     # NOTE: stas/c4-en-10k
@@ -78,7 +113,8 @@ def main():
         slow_but_exact=True,
         initializer_range=INITIALIZER_RANGE,
     )
-    model = BloomForCausalLM(config)
+    # model = BloomForCausalLM(config)
+    model = ToyModel(config)
 
     fp32_model = deepcopy(model).to("cuda")
     bf16_model = deepcopy(model).to("cuda")

tests/fp8/test_kernel.py
@@ -47,28 +47,28 @@ from timm.models.layers import trunc_normal_
         ),
     ],
 )
-# @pytest.mark.parametrize("dtype", [DTypes.FP8E4M3, DTypes.FP8E5M2])
-# @pytest.mark.parametrize("accum_dtype", [DTypes.KFLOAT16, DTypes.KFLOAT32])
-# @pytest.mark.parametrize("use_split_accumulator", [True, False])
+@pytest.mark.parametrize("dtype", [DTypes.FP8E4M3, DTypes.FP8E5M2])
+@pytest.mark.parametrize("accum_dtype", [DTypes.KFLOAT16, DTypes.KFLOAT32])
+@pytest.mark.parametrize("use_split_accumulator", [True, False])
 # def test_fp8_linear_forward_pass(input, dtype, accum_dtype, use_split_accumulator):
 # def test_fp8_matmul_kernel(input, weight, transpose_a, transpose_b, dtype, accum_dtype, use_split_accumulator):
-def test_fp8_matmul_kernel(input, weight, transpose_a, transpose_b):
-    dtype = DTypes.FP8E4M3
-    accum_dtype = DTypes.KFLOAT16
-    use_split_accumulator = False
+def test_fp8_matmul_kernel(input, weight, transpose_a, transpose_b, dtype, accum_dtype, use_split_accumulator):
+    # dtype = DTypes.FP8E4M3
+    # accum_dtype = DTypes.KFLOAT16
+    # use_split_accumulator = False
     # HIDDEN_SIZE = 64
     # INTERDIM_SIZE = 64 * 4
 
+    # trunc_normal_(input, std=0.02)
+    trunc_normal_(weight, std=0.02)
     ref_weight = weight
-    trunc_normal_(input, std=0.02)
-    trunc_normal_(ref_weight, std=0.02)
 
     fp8_input = deepcopy(input)
     fp8_weight = deepcopy(ref_weight)
     fp8_input = FP8Tensor(fp8_input, dtype)
     fp8_weight = FP8Tensor(fp8_weight, dtype)
 
-    ref_output = torch.matmul(input.T if transpose_a else input, ref_weight.T if transpose_b else ref_weight)
+    ref_output = torch.matmul(input.t() if transpose_a else input, ref_weight.t() if transpose_b else ref_weight)
     fp8_output = _fp8_matmul_kernel_2(
         mat_a=fp8_input,
         transpose_a=transpose_a,
@@ -82,4 +82,4 @@ def test_fp8_matmul_kernel(input, weight, transpose_a, transpose_b):
     assert fp8_output.dtype == QTYPE_TO_DTYPE[accum_dtype]
 
     # # NOTE: this threshold is from fp8-lm, the paper shows that this is fine
-    torch.testing.assert_allclose(ref_output, fp8_output, rtol=0, atol=0.1)
+    torch.testing.assert_allclose(fp8_output, ref_output, rtol=0, atol=0.1)

tests/fp8/test_linear.py
@@ -1,3 +1,4 @@
+import math
 from copy import deepcopy
 
 import pytest
@@ -76,14 +77,24 @@ def test_fp8_linear_forward_pass(input, is_bias, accum_qtype):
         torch.randn(64, 64, 64, device="cuda", dtype=torch.float32),  # [B, N, H]
     ],
 )
+@pytest.mark.parametrize(
+    "init_method",
+    [
+        lambda weight: trunc_normal_(weight, std=0.02),
+        lambda weight: trunc_normal_(weight, std=math.sqrt(1 / 64)),
+        lambda weight: trunc_normal_(weight, std=math.sqrt(1 / 64 * 4)),
+        lambda weight: trunc_normal_(weight, std=1),
+    ],
+)
 @pytest.mark.parametrize("accum_qtype", [DTypes.KFLOAT32, DTypes.KFLOAT16])
-def test_fp8_linear_backward_pass(input, accum_qtype):
+def test_fp8_linear_backward_pass(input, init_method, accum_qtype):
     HIDDEN_SIZE = 64
     INTERDIM_SIZE = 64 * 4
 
     ref_input = input.detach().clone().requires_grad_(True)
     ref_linear = nn.Linear(HIDDEN_SIZE, INTERDIM_SIZE, device="cuda", dtype=torch.float32)
-    trunc_normal_(ref_linear.weight, std=0.02)
+    # trunc_normal_(ref_linear.weight, std=0.02)
+    # trunc_normal_(ref_linear.weight, std=math.sqrt(1 / (HIDDEN_SIZE)))
 
     fp8_linear = convert_linear_to_fp8(deepcopy(ref_linear), accum_qtype)
 