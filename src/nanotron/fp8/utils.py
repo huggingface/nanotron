wip fixing overflow

src/nanotron/fp8/utils.py
@@ -1,7 +1,9 @@
+from typing import Dict, List, Tuple
+
+import pydevd
 import torch
 import transformer_engine as te  # noqa
 from torch import nn
-from typing import List, Tuple
 
 from nanotron.fp8.constants import FP8_GPU_NAMES, FP8LM_RECIPE, QTYPE_TO_DTYPE
 from nanotron.fp8.dtypes import DTypes
@@ -72,7 +74,7 @@ def get_leaf_modules(module: nn.Module) -> List[Tuple[str, nn.Module]]:
 
 def convert_to_fp8_module(module: nn.Module, accum_qtype: DTypes = FP8LM_RECIPE.linear.accum_dtype) -> nn.Module:
     def set_module(model, name, value):
-        parts = name.split('.')
+        parts = name.split(".")
         module = model
         for i, part in enumerate(parts):
             if part.isdigit():
@@ -86,7 +88,7 @@ def convert_to_fp8_module(module: nn.Module, accum_qtype: DTypes = FP8LM_RECIPE.
                 else:
                     module = getattr(module, part)
         return model
-        
+
     for name, child in get_leaf_modules(module):
         if isinstance(child, nn.Linear):
             fp8_linear = convert_linear_to_fp8(child, accum_qtype)
@@ -94,3 +96,42 @@ def convert_to_fp8_module(module: nn.Module, accum_qtype: DTypes = FP8LM_RECIPE.
             set_module(module, name, fp8_linear)
 
     return module
+
+
+def track_module_statistics(name: str, module: nn.Linear, logging: Dict[str, Dict[str, float]]):
+    if name not in logging:
+        logging[name] = {}
+
+    def _collect_stats(tensor):
+        return {
+            "mean": tensor.mean().item(),
+            "std": tensor.std().item(),
+            "var": tensor.var().item(),
+            "norm": tensor.norm().item(),
+            "min": tensor.min().item(),
+            "max": tensor.max().item(),
+        }
+
+    def _save_output_stats(module: nn.Linear, input: torch.Tensor, output: torch.Tensor):
+        logging[name]["weight"] = _collect_stats(module.weight)
+
+        if module.bias is not None:
+            logging[name]["bias"] = _collect_stats(module.bias)
+
+        logging[name]["output"] = _collect_stats(output)
+
+    def _save_grad_stats(module: nn.Linear, grad_output: torch.Tensor):
+        # if isinstance(grad_output, tuple):
+        #     for i, x in enumerate(tensor):
+        #         if x is None: continue
+
+        pydevd.settrace(suspend=False, trace_only_current_thread=True)
+        logging[name]["grad_output"] = _collect_stats(grad_output[0])
+
+    # def _save_grad_stats(module: nn.Linear, input_grad: torch.Tensor, grad_output: torch.Tensor):
+    #     pydevd.settrace(suspend=False, trace_only_current_thread=True)
+    #     logging[name]["grad_output"] = _collect_stats(grad_output)
+
+    module.register_forward_hook(_save_output_stats)
+    module.register_full_backward_pre_hook(_save_grad_stats)
+    # module.register_module_full_backward_hook(_save_grad_stats)

tests/fp8/reference_convergence/run_nanotron_fp8.py
@@ -1,13 +1,15 @@
 from copy import deepcopy
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
 import msamp
 import torch
 from datasets import load_dataset
+from nanotron.fp8.constants import FP8LM_RECIPE
 from nanotron.fp8.dtypes import DTypes
 from nanotron.fp8.loss_scaler import LossScaler
 from nanotron.fp8.optim import FP8Adam
 from nanotron.fp8.utils import convert_to_fp8_module
+from timm.models.layers import trunc_normal_
 from torch import nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
@@ -36,15 +38,13 @@ class ToyModel(nn.Module):
     def __init__(self, vocab_size, hidden_size):
         super().__init__()
         self.word_embeddings = nn.Embedding(vocab_size, hidden_size)
-        # self.net = nn.Sequential(
-        #     nn.Linear(config.hidden_size, config.vocab_size),
-        # )
         self.linear = nn.Linear(hidden_size, vocab_size)
 
-        # self.linear.weight.data.normal_(mean=0.0, std=0.02)
-        # self.word_embeddings.weight.data.normal_(mean=0.0, std=0.02)
-        # trunc_normal_(self.linear.weight.data, std=0.02)
-        # trunc_normal_(self.word_embeddings.weight.data, std=0.02)
+        import math
+
+        std = math.sqrt(1 / hidden_size)
+        trunc_normal_(self.word_embeddings.weight.data, std=std)
+        trunc_normal_(self.linear.weight.data, std=std)
 
     def forward(self, input_ids):
         x = self.word_embeddings(input_ids)
@@ -77,6 +77,8 @@ if __name__ == "__main__":
     # assert 16 ** math.ceil(math.log(tokenizer.vocab_size, 16)) == tokenizer.vocab_size
     assert len(tokenizer) % 16 == 0
 
+    torch.cuda.empty_cache()
+
     # fp32_linear = nn.Sequential(
     #     *[
     #         layer
@@ -97,16 +99,16 @@ if __name__ == "__main__":
     # fp32_linear = fp32_linear[:-1]
     fp32_linear = ToyModel(vocab_size=len(tokenizer), hidden_size=HIDDEN_SIZE).to("cuda")
 
-    bf16_linear = deepcopy(fp32_linear)
-    fp8_linear = deepcopy(fp32_linear)
+    # bf16_linear = deepcopy(fp32_linear)
+    # fp8_linear = deepcopy(fp32_linear)
     fp8_linear_with_scaler = deepcopy(fp32_linear)
     msamp_linear = deepcopy(fp32_linear)
     msamp_linear_with_scaler = deepcopy(fp32_linear)
 
     fp32_optim = Adam(fp32_linear.parameters(), lr=LR)
 
-    bf16_linear = bf16_linear.to(dtype=torch.bfloat16)
-    bf16_optim = Adam(bf16_linear.parameters(), lr=LR)
+    # bf16_linear = bf16_linear.to(dtype=torch.bfloat16)
+    # bf16_optim = Adam(bf16_linear.parameters(), lr=LR)
 
     msamp_optim = Adam(msamp_linear.parameters(), lr=LR)
     msamp_linear, msamp_optim = msamp.initialize(msamp_linear, msamp_optim, opt_level="O2")
@@ -116,8 +118,8 @@ if __name__ == "__main__":
         msamp_linear_with_scaler, msamp_optim_with_scaler, opt_level="O2"
     )
 
-    fp8_linear = convert_to_fp8_module(fp8_linear, accum_qtype=DTypes.KFLOAT16)
-    fp8_optim = FP8Adam(fp8_linear.parameters(), lr=LR)
+    # fp8_linear = convert_to_fp8_module(fp8_linear, accum_qtype=DTypes.KFLOAT16)
+    # fp8_optim = FP8Adam(fp8_linear.parameters(), lr=LR)
 
     fp8_linear_with_scaler = convert_to_fp8_module(fp8_linear_with_scaler, accum_qtype=DTypes.KFLOAT16)
     fp8_optim_with_scaler = FP8Adam(fp8_linear_with_scaler.parameters(), lr=LR)
@@ -137,6 +139,7 @@ if __name__ == "__main__":
             "optim": fp32_optim.__class__.__name__,
             "optim_params": fp32_optim.defaults,
             "num_params": sum(p.numel() for p in fp32_linear.parameters()),
+            "fp8_recipe": asdict(FP8LM_RECIPE),
         },
     )
 
@@ -177,10 +180,10 @@ if __name__ == "__main__":
             inputs = batch["input_ids"]
             targets = batch["input_ids"][:, 1:].contiguous()
 
-            # inputs = batch_inputs[step]
-            # targets = batch_targets[step]
-            # inputs = torch.randn(BATCH_SIZE, INPUT_DIM).to("cuda")
-            # targets = torch.randint(0, HIDDEN_SIZE, (BATCH_SIZE,)).to("cuda")
+            # # inputs = batch_inputs[step]
+            # # targets = batch_targets[step]
+            inputs = torch.randn(BATCH_SIZE, INPUT_DIM).to("cuda")
+            targets = torch.randint(0, HIDDEN_SIZE, (BATCH_SIZE,)).to("cuda")
 
             fp32_optim.zero_grad()
             ref_output = fp32_linear(inputs)
@@ -203,7 +206,7 @@ if __name__ == "__main__":
             fp8_optim_with_scaler.zero_grad()
             fp8_output_with_scaler = fp8_linear_with_scaler(inputs)
             fp8_loss_with_scaler = loss_func(fp8_output_with_scaler, targets)
-            fp8_scaler.scale(fp8_loss_with_scaler)
+            fp8_scaler.scale(fp8_loss_with_scaler).backward()
             fp8_scaler.step(fp8_optim_with_scaler)
             fp8_scaler.update()
 

tests/fp8/test_fp8_utils.py
@@ -3,7 +3,13 @@ import torch
 from nanotron.fp8.dtypes import DTypes
 from nanotron.fp8.linear import FP8Linear
 from nanotron.fp8.parameter import FP8Parameter
-from nanotron.fp8.utils import convert_linear_to_fp8, convert_to_fp8_module, is_overflow_underflow_nan, get_leaf_modules
+from nanotron.fp8.utils import (
+    convert_linear_to_fp8,
+    convert_to_fp8_module,
+    get_leaf_modules,
+    is_overflow_underflow_nan,
+    track_module_statistics,
+)
 from torch import nn
 
 
@@ -22,7 +28,7 @@ def test_convert_linear_to_fp8(accum_dtype):
 def test_convert_module_to_fp8(model_type, accum_dtype):
     HIDDEN_SIZE = 16
     input = torch.randn(1, HIDDEN_SIZE, device="cuda")
-    
+
     if model_type == "multilayers":
         model = nn.Sequential(
             nn.Linear(16, 16, device="cuda"),
@@ -32,17 +38,17 @@ def test_convert_module_to_fp8(model_type, accum_dtype):
         )
     else:
         model = nn.Transformer(nhead=2, num_encoder_layers=4, d_model=HIDDEN_SIZE).to("cuda")
-    
+
     fp8_model = convert_to_fp8_module(model, accum_dtype)
-    
+
     assert fp8_model(input).shape == model(input).shape
 
     ref_modules = get_leaf_modules(model)
     fp8_modules = get_leaf_modules(fp8_model)
-    
+
     for (ref_name, ref_module), (fp8_name, fp8_module) in zip(ref_modules, fp8_modules):
         assert ref_name == fp8_name
-        
+
         if not isinstance(ref_module, nn.Linear):
             assert isinstance(fp8_module, type(ref_module))
         else:
@@ -72,3 +78,14 @@ def test_convert_module_to_fp8(model_type, accum_dtype):
 def test_detect_overflow_underflow_nan(tensor, expected_output):
     output = is_overflow_underflow_nan(tensor)
     assert output == expected_output
+
+
+def test_track_module_statistics():
+    input = torch.randn(1, 16, device="cuda")
+    linear = nn.Linear(16, 16, device="cuda")
+
+    LOGGING = {}
+    track_module_statistics("linear", linear, logging=LOGGING)
+    linear(input).sum().backward()
+
+    assert 1 == 1

tests/fp8/test_linear.py
@@ -1,5 +1,6 @@
 import math
 from copy import deepcopy
+from functools import partial, reduce
 
 import pytest
 import torch
@@ -8,7 +9,7 @@ from nanotron.fp8.dtypes import DTypes
 from nanotron.fp8.linear import FP8Linear
 from nanotron.fp8.parameter import FP8Parameter
 from nanotron.fp8.tensor import FP8Tensor, convert_tensor_from_fp8
-from nanotron.fp8.utils import convert_linear_to_fp8
+from nanotron.fp8.utils import convert_linear_to_fp8, convert_to_fp8_module
 from timm.models.layers import trunc_normal_
 from torch import nn
 
@@ -115,6 +116,51 @@ def test_fp8_linear_backward_pass(input, init_method, accum_qtype):
     weight_grad = convert_tensor_from_fp8(fp8_linear.weight.grad, fp8_linear.weight.grad.fp8_meta, torch.float32)
     torch.testing.assert_allclose(weight_grad, ref_linear.weight.grad, rtol=0.06, atol=0.1)
     torch.testing.assert_allclose(fp8_linear.bias.grad, ref_linear.bias.grad)
+
+
+@pytest.mark.parametrize("accum_qtype", [DTypes.KFLOAT32, DTypes.KFLOAT16])
+def test_fp8_modules_trigger_the_entire_computational_graph(accum_qtype):
+    HIDDEN_SIZE = 16
+    TIMELINE = []
+
+    def backward_hook(module, grad_input, grad_output, idx):
+        TIMELINE.append(f"{module.__class__.__name__}.{idx}.backward")
+
+    class Logger(nn.Module):
+        def __init__(self, idx: int, module: nn.Linear):
+            super().__init__()
+            module.register_backward_hook(partial(backward_hook, idx=idx))
+            self.module = module
+            self.idx = idx
+
+        def forward(self, input):
+            TIMELINE.append(f"{self.module.__class__.__name__}.{self.idx}.forward")
+            return self.module(input)
+
+    input = torch.randn(HIDDEN_SIZE, HIDDEN_SIZE, device="cuda", dtype=torch.float32)
+    fp8_linear = nn.Sequential(
+        nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE, device="cuda", dtype=torch.float32),
+        nn.ReLU(),
+        nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE, device="cuda", dtype=torch.float32),
+        nn.ReLU(),
+    )
+    fp8_linear = convert_to_fp8_module(fp8_linear, accum_qtype)
+    fp8_linear = nn.ModuleList([Logger(idx, module) for idx, module in enumerate(fp8_linear)])
+
+    output = reduce(lambda x, module: module(x), fp8_linear, input)
+    scalar = torch.randn(1, device="cuda", dtype=output.dtype)
+    (output.sum() * scalar).backward()
+
+    assert TIMELINE == [
+        "FP8Linear.0.forward",
+        "ReLU.1.forward",
+        "FP8Linear.2.forward",
+        "ReLU.3.forward",
+        "ReLU.3.backward",
+        "FP8Linear.2.backward",
+        "ReLU.1.backward",
+        "FP8Linear.0.backward",
+    ]
 
 
 # NOTE: it seems that dynamic quantization should be in test_tensor