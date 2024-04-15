fixed overflow due to calculate grad_input incorrectly

src/nanotron/fp8/linear.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple, Union, TypedDict
+from typing import Optional, Tuple, Union, TypedDict, cast
 
 import torch
 import torch.nn.functional as F
@@ -100,7 +100,7 @@ class _FP8Matmul(torch.autograd.Function):
             )
 
         ctx.accum_qtype = accum_qtype
-        ctx.save_for_backward(input, weight)
+        ctx.save_for_backward(input, weight.data)
 
         # NOTE: pass FP8Tensor instead of FP8Parameter
         output = fp8_matmul_kernel(
@@ -133,13 +133,12 @@ class _FP8Matmul(torch.autograd.Function):
         ∂L/∂W = Xᵀ @ ∂L/∂Y
         Reference: https://web.eecs.umich.edu/~justincj/teaching/eecs442/notes/linear-backprop.html
         """
-        pydevd.settrace(suspend=False, trace_only_current_thread=True)
+        # pydevd.settrace(suspend=False, trace_only_current_thread=True)
         fp8_input, fp8_weight = ctx.saved_tensors
         accum_qtype = ctx.accum_qtype
-
-        # if type(grad_output) == torch.Tensor:
         
-        #     # TODO(xrsrke): investigate how does grad_output.contiguous() affect the outputs
+        fp8_input = cast(FP8Tensor, fp8_input)
+        fp8_weight = cast(FP8Tensor, fp8_weight)
         grad_output = grad_output.contiguous()
         fp8_grad_output = FP8Tensor(grad_output, dtype=FP8LM_RECIPE.linear.output_grad.dtype)
 
@@ -165,7 +164,7 @@ class _FP8Matmul(torch.autograd.Function):
 
         # fp8_weight_transposed = tex.fp8_transpose(fp8_weight, fp8_weight.fp8_meta.te_dtype)
         # fp8_weight_transposed.fp8_meta = fp8_weight.fp8_meta
-        transposed_fp8_weight = fp8_weight.t()
+        transposed_fp8_weight = fp8_weight.transpose_fp8()
 
         grad_input = fp8_matmul_kernel(
             mat_a=transposed_fp8_weight,
@@ -174,7 +173,7 @@ class _FP8Matmul(torch.autograd.Function):
             transpose_b=False,
             use_split_accumulator=FP8LM_RECIPE.linear.split_accumulator.input_grad,
             accum_qtype=accum_qtype,
-            # is_backward=True
+            is_backward=True
         )
         
         # fp8_grad_output_transposed = tex.fp8_transpose(fp8_grad_output, fp8_grad_output.fp8_meta.te_dtype)
@@ -182,8 +181,8 @@ class _FP8Matmul(torch.autograd.Function):
         # fp8_input_tranposed = tex.fp8_transpose(fp8_input, fp8_input.fp8_meta.te_dtype)
         # fp8_input_tranposed.fp8_meta = fp8_input.fp8_meta
         
-        transposed_fp8_grad_output = fp8_grad_output.t()
-        transposed_fp8_input = fp8_input.t()
+        transposed_fp8_grad_output = fp8_grad_output.transpose_fp8()
+        transposed_fp8_input = fp8_input.transpose_fp8()
 
         grad_weight = fp8_matmul_kernel(
             mat_a=transposed_fp8_input,

src/nanotron/fp8/tensor.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from abc import abstractstaticmethod
 from copy import deepcopy
-from typing import Union, cast
+from typing import Union, cast, Optional
 
 import torch
 import transformer_engine as te  # noqa
@@ -10,22 +10,33 @@ import transformer_engine_extensions as tex
 
 from nanotron.fp8.constants import DTYPE_TO_FP8_MAX, FP8_DTYPES, INITIAL_SCALING_FACTOR
 from nanotron.fp8.dtypes import DTypes
+from nanotron.fp8.meta import FP8Meta
 
 
 class LowPrecisionTensor(torch.Tensor):
     def __new__(
-        cls, tensor: torch.Tensor, dtype: DTypes, interval: int = 1, is_dynamic_scaling: bool = False
+        cls,
+        tensor: torch.Tensor,
+        dtype: Optional[DTypes] = None,
+        interval: Optional[int] = 1,
+        is_dynamic_scaling: Optional[bool] = False,
+        fp8_meta: Optional[FP8Meta] = None
     ) -> torch.Tensor:
         assert isinstance(tensor, torch.Tensor), "tensor must be a tensor"
-        assert tensor.dtype not in FP8_DTYPES, "The tensor already quantized to FP8"
 
         # TODO(xrsrke): if the tensor is on cpu, then bypass the quantization
         # because the current kernels only support gpu tensor
         assert tensor.device != torch.device("cpu"), "FP8Tensor only supports CUDA device"
-        assert dtype in [DTypes.FP8E4M3, DTypes.FP8E5M2, DTypes.KFLOAT16]
 
-        fp8_meta = cls._get_metadata(tensor, dtype, interval, is_dynamic_scaling)
-        fp8_tensor = cls._quantize(tensor, fp8_meta)
+        if fp8_meta is None:
+            assert tensor.dtype not in FP8_DTYPES, "The tensor already quantized to FP8"
+            assert dtype in [DTypes.FP8E4M3, DTypes.FP8E5M2, DTypes.KFLOAT16]
+            
+            fp8_meta = cls._get_metadata(tensor, dtype, interval, is_dynamic_scaling)
+            fp8_tensor = cls._quantize(tensor, fp8_meta)
+        else:
+            assert tensor.dtype in FP8_DTYPES
+            fp8_tensor = tensor
 
         # TODO(xrsrke): move update inverse scaling to FP8Meta's initialization
         obj = torch.Tensor._make_subclass(cls, fp8_tensor)
@@ -65,7 +76,7 @@ class LowPrecisionTensor(torch.Tensor):
 
     @abstractstaticmethod
     def _quantize(tensor: torch.Tensor, fp8_meta: "FP8Meta") -> torch.Tensor:
-        raise NotImplementedError
+        ...
 
     def mul_(self, other: torch.Tensor):
         from nanotron.fp8.meta import FP8Meta
@@ -117,13 +128,11 @@ class LowPrecisionTensor(torch.Tensor):
 
         self.fp8_meta.add_amax(new_amax)
     
-    @property
-    def T(self) -> FP8Tensor:
+    def transpose_fp8(self) -> FP8Tensor:
         """Transpose the tensor."""
         transposed_t = tex.fp8_transpose(self, self.fp8_meta.te_dtype)
         transposed_t.fp8_meta = self.fp8_meta
-        # return FP8Tensor(transposed_t, self.fp8_meta.dtype, self.fp8_meta.interval)
-        return transposed_t
+        return self.__class__(transposed_t, fp8_meta=self.fp8_meta)
 
     def __repr__(self) -> str:
         if hasattr(self, "fp8_meta"):

tests/fp8/reference_convergence/run_nanotron_cifar.py
@@ -219,7 +219,7 @@ if __name__ == "__main__":
             fp8_with_scaler_logs = _log(fp8_linear_with_scaler)
             fp8_output_with_scaler = fp8_linear_with_scaler(inputs)
             fp8_loss_with_scaler = loss_func(fp8_output_with_scaler, targets)
-            fp8_scaler.scaling_value = deepspeed_linear.optimizer.loss_scaler.loss_scale
+            fp8_scaler.scaling_value = torch.tensor(deepspeed_linear.optimizer.loss_scaler.loss_scale, device="cuda")
             scaled_fp8_loss_with_scaler = fp8_scaler.scale(fp8_loss_with_scaler)
             fp8_optim_with_scaler.zero_grad()
             scaled_fp8_loss_with_scaler.backward()

tests/fp8/test_tensor.py
@@ -282,7 +282,7 @@ def test_transpose_fp8_tensor(tensor_cls, dtype):
     tensor = torch.randn((16, 16), dtype=torch.float32, device="cuda:0")
     fp8_tensor = tensor_cls(deepcopy(tensor), dtype)
 
-    transposed_fp8_tensor = fp8_tensor.T
+    transposed_fp8_tensor = fp8_tensor.transpose_fp8()
     if tensor_cls == FP8Tensor:
         assert isinstance(transposed_fp8_tensor, FP8Tensor)
         ref_transposed = convert_tensor_from_fp8(fp8_tensor, fp8_tensor.fp8_meta, torch.float32).T
@@ -295,7 +295,7 @@ def test_transpose_fp8_tensor(tensor_cls, dtype):
     assert torch.equal(dequant_transposed_fp8_tensor, ref_transposed)
     
     # NOTE: transpose should not affect quantization
-    detransposed_fp8_tensor = transposed_fp8_tensor.T
+    detransposed_fp8_tensor = transposed_fp8_tensor.transpose_fp8()
     
     assert 1 == 1
 