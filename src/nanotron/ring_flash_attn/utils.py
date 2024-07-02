9208928b473b68e06f1b1f0a3aa9986d381f8ef7
zzhhjjj <z785566960@gmail.com>
2024-07-02 14:31:56 +0000

    clean code

--- src/nanotron/ring_flash_attn/utils.py
+++ src/nanotron/ring_flash_attn/utils.py
@@ -110,60 +110,6 @@ class RingComm:
         self._ops = []
 
 
-###### my implementation
-# class RingComm:
-#     def __init__(self, process_group: dist.ProcessGroup):
-#         self._process_group = process_group
-#         self._ops = []
-#         self.rank = dist.get_rank(self._process_group)
-#         self.world_size = dist.get_world_size(self._process_group)
-#         self._reqs = []
-
-#         self.send_rank = (self.rank + 1) % self.world_size
-#         self.recv_rank = (self.rank - 1) % self.world_size
-#         self.send_next_next_rank = (self.rank + 2) % self.world_size
-#         self.recv_next_next_rank = (self.rank - 2) % self.world_size
-
-#         if process_group is not None:
-#             self.send_rank = dist.get_global_rank(self._process_group, self.send_rank)
-#             self.recv_rank = dist.get_global_rank(self._process_group, self.recv_rank)
-#             self.send_next_next_rank = dist.get_global_rank(self._process_group, self.send_next_next_rank)
-#             self.recv_next_next_rank = dist.get_global_rank(self._process_group, self.recv_next_next_rank)
-
-#     def send_recv(self, to_send: torch.Tensor, recv_tensor: Optional[torch.Tensor] = None, next_next=False) -> torch.Tensor:
-#         if recv_tensor is None:
-#             res = torch.empty_like(to_send)
-#         else:
-#             res = recv_tensor
-#         if next_next:
-#             send_op = dist.P2POp(dist.isend, to_send, self.send_next_next_rank, group=self._process_group)
-#             recv_op = dist.P2POp(dist.irecv, res, self.recv_next_next_rank, group=self._process_group)
-#         else:
-#             send_op = dist.P2POp(dist.isend, to_send, self.send_rank, group=self._process_group)
-#             recv_op = dist.P2POp(dist.irecv, res, self.recv_rank, group=self._process_group)
-#         self._ops.append(send_op)
-#         self._ops.append(recv_op)
-#         return res
-
-#     def commit(self):
-#         # if self._reqs is not None:
-#         #     raise RuntimeError("commit called twice")
-#         self._reqs.extend(dist.batch_isend_irecv(self._ops))
-
-#     def wait(self):
-#         if self._reqs is None:
-#             raise RuntimeError("wait called before commit")
-#         # for req in self._reqs:
-#         #     req.wait()
-#         # self._reqs = None
-
-#         # use more buffer. This may help
-#         for req in self._reqs[:2]:  # Only wait for the first two requests
-#             req.wait()
-#         self._reqs = self._reqs[2:]  # Remove the first two requests from the list
-#         self._ops = []
-
-
 def extract_local(tensor: torch.Tensor, rank: int, world_size: int, dim: int = 1) -> torch.Tensor:
     value_chunks = tensor.chunk(2 * world_size, dim=dim)
     local_value = torch.cat([value_chunks[rank], value_chunks[2 * world_size - rank - 1]], dim=dim)
