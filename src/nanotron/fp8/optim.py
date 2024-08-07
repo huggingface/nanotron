backup and fix attn entropy calculation

src/nanotron/fp8/optim.py
@@ -112,7 +112,8 @@ class Adam(Optimizer):
                 from nanotron import constants
                 from nanotron.constants import get_debug_save_path
 
-                if constants.is_ready_to_log is True and constants.CONFIG.logging.monitor_model_states is True:
+                # if constants.is_ready_to_log is True and constants.CONFIG.logging.monitor_model_states is True:
+                if constants.is_ready_to_log is True:
                     # debug_save_path = constants.DEBUG_SAVE_PATH.format(constants.CONFIG.general.run, constants.ITERATION_STEP)
                     debug_save_path = get_debug_save_path(constants.CONFIG.general.run, constants.ITERATION_STEP)
 
@@ -479,7 +480,13 @@ class FP8Adam(Optimizer):
                     if "0.pp_block.mlp.down_proj.weight" in p_name:
                         assert 1 == 1
 
-                rms = self._calculate_mean_sqrt_ignoring_nans(fp32_grad.pow(2), unbiased_fp32_exp_avg_sq)
+                rms = self._calculate_mean_sqrt_ignoring_nans(
+                    fp32_grad.pow(2),
+                    torch.max(
+                        unbiased_fp32_exp_avg_sq,
+                        torch.tensor(group["eps"], dtype=self.optim_accum_dtype, device="cuda").pow(2),
+                    ),
+                )
 
                 # log_rank(
                 #     f"[Gradient clipping] The RMS is {rms}",  # noqa
@@ -500,12 +507,14 @@ class FP8Adam(Optimizer):
                 else:
                     update_lr = lr
 
-                weight_decay_factor = group["weight_decay"] if data.ndim >= 2 else 0.
+                weight_decay_factor = group["weight_decay"] if data.ndim >= 2 else 0.0
 
                 if weight_decay_factor != 0:
-                    new_fp32_data = fp32_data - update_lr * (normalized_grad + weight_decay_factor * fp32_data)
+                    fp32_new_changes_in_p = update_lr * (normalized_grad + weight_decay_factor * fp32_data)
                 else:
-                    new_fp32_data = fp32_data - update_lr * normalized_grad
+                    fp32_new_changes_in_p = update_lr * normalized_grad
+
+                new_fp32_data = fp32_data - fp32_new_changes_in_p
 
                 if IS_FP8:
                     # self.mappping_fp8_to_master_weight[p] = FP16Tensor(new_fp32_data, dtype=DTypes.KFLOAT16)
@@ -555,24 +564,30 @@ class FP8Adam(Optimizer):
                 assert state["exp_avg"] is fp32_exp_avg
                 assert state["exp_avg_sq"] is fp32_exp_avg_sq
 
-                loggings[p]["step"] = {"value": step}
-                loggings[p]["group:lr"] = {"value": lr}
-                loggings[p]["group:eps"] = {"value": group["eps"]}
-                loggings[p]["group:beta1"] = {"value": beta1}
-                loggings[p]["group:beta2"] = {"value": beta2}
+                if constants.is_ready_to_log is True:
+                    loggings[p]["step"] = {"value": step}
+                    loggings[p]["group:lr"] = {"value": lr}
+                    loggings[p]["group:eps"] = {"value": group["eps"]}
+                    loggings[p]["group:beta1"] = {"value": beta1}
+                    loggings[p]["group:beta2"] = {"value": beta2}
 
-                loggings[p]["bias_correction1"] = {"value": bias_correction1}
-                loggings[p]["bias_correction2"] = {"value": bias_correction2}
-                loggings[p]["fp32_exp_avg"] = compute_stas(fp32_exp_avg)
-                loggings[p]["fp32_exp_avg_sq"] = compute_stas(fp32_exp_avg_sq)
+                    loggings[p]["bias_correction1"] = {"value": bias_correction1}
+                    loggings[p]["bias_correction2"] = {"value": bias_correction2}
+                    loggings[p]["fp32_exp_avg"] = compute_stas(fp32_exp_avg)
+                    loggings[p]["fp32_exp_avg_sq"] = compute_stas(fp32_exp_avg_sq)
 
-                loggings[p]["normalized_grad"] = compute_stas(normalized_grad)
-                loggings[p]["denom"] = compute_stas(denom)
-                loggings[p]["grad_rms"] = {"value": rms}
-                loggings[p]["update_lr"] = {"value": update_lr}
+                    loggings[p]["normalized_grad"] = compute_stas(normalized_grad)
+                    loggings[p]["denom"] = compute_stas(denom)
+                    loggings[p]["grad_rms"] = {"value": rms}
+                    loggings[p]["update_lr"] = {"value": update_lr}
 
-                loggings[p]["fp32_p"] = compute_stas(fp32_data)
-                loggings[p]["fp32_grad"] = compute_stas(fp32_grad)
+                    loggings[p]["fp32_p"] = compute_stas(fp32_data)
+                    loggings[p]["fp32_new_changes_in_p"] = {
+                        "abs_total": fp32_new_changes_in_p.abs().sum(),
+                        "abs_mean": fp32_new_changes_in_p.abs().mean(),
+                    }
+                    loggings[p]["fp32_grad"] = compute_stas(fp32_grad)
+                    loggings[p]["update_lr"] = {"value": update_lr}
 
         self.loggings = loggings
 

src/nanotron/models/llama.py
@@ -45,6 +45,7 @@ from nanotron.parallel.tensor_parallel.nn import (
     TensorParallelRowLinear,
 )
 from nanotron.random import RandomStates
+from nanotron.scaling.monitor import log_to_nn_state_dict
 from nanotron.scaling.parametrization import SpectralMupParametrizator, StandardParametrizator
 from nanotron.utils import checkpoint_method
 
@@ -213,6 +214,38 @@ class MLP(nn.Module):
         return {"hidden_states": hidden_states}
 
 
+# def _compute_attention_entropy(probs):
+#     # Compute entropy
+#     ent = -probs * torch.log(probs)  # Add small epsilon to avoid log(0)
+
+#     # Compute the entropy of each word's probs
+#     ent = ent.sum(dim=-1)  # [batch_size, n_heads, q_length]
+
+#     # Compute the mean entropy across heads and words
+#     ent = ent.mean(dim=(-2, -1))  # [batch_size]
+
+#     return ent
+
+
+def calculate_attention_entropy(attention_probs):
+    # attention_probs.shape = [batch_size, n_heads, seq_len, seq_len]
+    # batch_size, n_heads, seq_len, _ = attention_probs.shape
+
+    # Calculate entropy for each row
+    row_entropy = -torch.sum(attention_probs * torch.log(attention_probs + 1e-7), dim=-1)
+    # row_entropy.shape = [batch_size, n_heads, seq_len]
+
+    # Average entropy across sequence length
+    avg_entropy_per_head = torch.mean(row_entropy, dim=-1)
+    # avg_entropy_per_head.shape = [batch_size, n_heads]
+
+    # Average entropy across heads
+    avg_entropy = torch.mean(avg_entropy_per_head, dim=-1)
+    # avg_entropy.shape = [batch_size]
+
+    return avg_entropy
+
+
 def self_attention_with_causal_mask(
     query_states: torch.Tensor,  # [batch_size * q_length, n_local_q_heads, inner_dim]
     key_states: torch.Tensor,  # [batch_size * kv_length, n_local_kv_heads, inner_dim]
@@ -286,7 +319,7 @@ def self_attention_with_causal_mask(
     # Reshape output to match input shape
     output = output.transpose(1, 2).contiguous().view(batch_size * q_length, n_local_q_heads, inner_dim)
 
-    return output
+    return (attn_weights, raw_attn_probs, attn_probs, output)
 
 
 class CoreAttention(nn.Module):
@@ -300,6 +333,7 @@ class CoreAttention(nn.Module):
         self.d_v = config.hidden_size // config.num_attention_heads
         self.is_using_mup = config.is_using_mup
 
+        self.layer_idx = layer_idx
         self.checkpoint_attention = False  # Because flash_attn already does checkpointing
 
     @checkpoint_method(attr_name="checkpoint_attention")
@@ -340,7 +374,8 @@ class CoreAttention(nn.Module):
         # )
         batch_size = query_states.shape[0] // constants.CONFIG.tokens.sequence_length
 
-        attn_output = self_attention_with_causal_mask(
+        # attn_weights, raw_attn_probs, attn_probs, output
+        attn_weights, raw_attn_probs, attn_probs, attn_output = self_attention_with_causal_mask(
             query_states=query_states,
             key_states=key_states,
             value_states=value_states,
@@ -348,6 +383,37 @@ class CoreAttention(nn.Module):
             q_length=constants.CONFIG.tokens.sequence_length,
             kv_length=constants.CONFIG.tokens.sequence_length,
         )
+
+        from nanotron.fp8.utils import compute_stas
+
+        if constants.is_ready_to_log is True:
+            log_to_nn_state_dict(
+                f"model.decoder.{self.layer_idx}.pp_block.attn.attn_weights", compute_stas(attn_weights)
+            )
+            log_to_nn_state_dict(
+                f"model.decoder.{self.layer_idx}.pp_block.attn.raw_attn_probs", compute_stas(raw_attn_probs)
+            )
+            log_to_nn_state_dict(f"model.decoder.{self.layer_idx}.pp_block.attn.attn_probs", compute_stas(attn_probs))
+
+            # def mean_ignore_nan(tensor):
+            #     tensor_without_nan = tensor[~torch.isnan(tensor)]
+            #     return torch.mean(tensor_without_nan)
+
+            # attn_entropy = _compute_attention_entropy(attn_probs)  # [seq_length, ]
+            # attn_entropy = mean_ignore_nan(attn_entropy)
+            raw_attn_entropy = calculate_attention_entropy(raw_attn_probs).mean()
+            log_to_nn_state_dict(
+                f"model.decoder.{self.layer_idx}.pp_block.attn.raw_attn_entropy", {"value": raw_attn_entropy}
+            )
+
+            from nanotron.constants import CONFIG
+
+            if CONFIG.fp8.clipped_softmax is True:
+                clipped_attn_entropy = calculate_attention_entropy(attn_probs).mean()
+                log_to_nn_state_dict(
+                    f"model.decoder.{self.layer_idx}.pp_block.attn.clipped_attn_entropy",
+                    {"value": clipped_attn_entropy},
+                )
 
         return attn_output
 
@@ -378,43 +444,6 @@ def pad_to_right(tensor, mask, new_tensor=None):
     # We fill the new tensor with the useful values
     new_tensor[:, : right_padded_mask.shape[1], :, :][right_padded_mask] = useful_values
     return new_tensor, right_padded_mask
-
-
-def _compute_attention_entropy(q, k):
-    """
-    x: [batch_size, seq_len, d_model]
-    q: [batch_size * q_length, n_heads, d_qk]
-    k: [batch_size * kv_length, n_heads, d_qk]
-    """
-    import math
-
-    import torch.nn.functional as F
-
-    from nanotron import constants
-
-    # Reshape q and k to separate batch_size and seq_len dimensions
-    batch_size = q.shape[0] // constants.CONFIG.tokens.sequence_length
-
-    q = q.view(batch_size, -1, q.shape[1], q.shape[2])  # [batch_size, q_length, n_heads, d_qk]
-    k = k.view(batch_size, -1, k.shape[1], k.shape[2])  # [batch_size, kv_length, n_heads, d_qk]
-
-    # Compute attention scores
-    scores = torch.einsum("bqhd,bkhd->bhqk", q, k)  # [batch_size, n_heads, q_length, kv_length]
-    scores /= math.sqrt(q.shape[2])
-
-    # Apply softmax to get probabilities
-    probs = F.softmax(scores, dim=-1)
-
-    # Compute entropy
-    ent = -probs * torch.log(probs + 1e-9)  # Add small epsilon to avoid log(0)
-
-    # Compute the entropy of each word's probs
-    ent = ent.sum(dim=-1)  # [batch_size, n_heads, q_length]
-
-    # Compute the mean entropy across heads and words
-    ent = ent.mean(dim=(-2, -1))  # [batch_size]
-
-    return ent
 
 
 class CausalSelfAttention(nn.Module, AttachableStore):
@@ -774,23 +803,6 @@ class CausalSelfAttention(nn.Module, AttachableStore):
             attention_output.contiguous().view(batch_size, q_length, self.n_local_q_heads * self.d_v).transpose(0, 1)
         )
         output = self.o_proj(attention_output)
-        attn_entropy = _compute_attention_entropy(query_states, key_states)  # [seq_length, ]
-
-        def mean_ignore_nan(tensor):
-            # Remove NaN values
-            tensor_without_nan = tensor[~torch.isnan(tensor)]
-
-            # Calculate mean if there are non-NaN values, otherwise return NaN
-            return torch.mean(tensor_without_nan)
-
-        if constants.CONFIG.logging.monitor_model_states is True:
-            # attn_entropy = attn_entropy.mean()
-            attn_entropy = mean_ignore_nan(attn_entropy)
-
-            # if not self.name in constants.NN_STATES:
-            #     constants.NN_STATES[self.name] = {}
-
-            constants.NN_STATES[self.name]["attn_entropy"] = {"value": attn_entropy}
 
         return {"hidden_states": output, "sequence_mask": sequence_mask}
 
@@ -805,6 +817,7 @@ class LlamaDecoderLayer(nn.Module):
     ):
         super().__init__()
         # self.input_layernorm = TritonRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.layer_idx = layer_idx
         self.normalized_shape = (config.hidden_size,)
         self.input_layernorm = nn.LayerNorm(config.hidden_size, eps=config.rms_norm_eps)
         # self.input_layernorm = nn.Identity()
@@ -822,6 +835,18 @@ class LlamaDecoderLayer(nn.Module):
         # self.post_attention_layernorm = nn.Identity()
         self.mlp = MLP(config=config, parallel_config=parallel_config, tp_pg=tp_pg, layer_idx=layer_idx)
 
+        from nanotron.constants import CONFIG
+
+        if CONFIG.fp8.layer_scale is True:
+            if CONFIG.fp8.layer_scale_init == "zeros":
+                attn_layer_scale_init = torch.zeros(config.hidden_size)
+                mlp_layer_scale_init = torch.zeros(config.hidden_size)
+            else:
+                raise NotImplementedError(f"Unsupported layer_scale_init: {CONFIG.fp8.layer_scale_init}")
+
+            self.attn_layer_scale = torch.nn.Parameter(attn_layer_scale_init)
+            self.mlp_layer_scale = torch.nn.Parameter(mlp_layer_scale_init)
+
     def forward(
         self,
         hidden_states: Union[torch.Tensor, TensorPointer],
@@ -836,6 +861,40 @@ class LlamaDecoderLayer(nn.Module):
 
         output = self.attn(hidden_states=hidden_states, sequence_mask=sequence_mask)
         hidden_states = output["hidden_states"]
+
+        from einops import rearrange
+
+        from nanotron.constants import CONFIG
+        from nanotron.fp8.utils import compute_stas
+
+        if constants.is_ready_to_log is True:
+            log_to_nn_state_dict(
+                f"model.decoder.{self.layer_idx}.pp_block.attn.without_resid.before_layerscale",
+                compute_stas(hidden_states),
+            )
+            log_to_nn_state_dict(f"model.decoder.{self.layer_idx}.pp_block.attn.resid", compute_stas(residual))
+            log_to_nn_state_dict(
+                f"model.decoder.{self.layer_idx}.pp_block.attn_and_resid_ratio.before_layerscale",
+                {"value": hidden_states.norm(p=2) / residual.norm(p=2)},
+            )
+
+        if CONFIG.fp8.layer_scale is True:
+            hidden_states = hidden_states * rearrange(self.attn_layer_scale.data, "f -> 1 1 f")
+            if constants.is_ready_to_log is True:
+                # NN_STATES[f"model.decoder.{self.layer_idx}.pp_block.attn.layerscale"] = {"absmax": self.attn_layer_scale.abs().max()}
+                log_to_nn_state_dict(
+                    f"model.decoder.{self.layer_idx}.pp_block.attn.layerscale",
+                    compute_stas(self.attn_layer_scale.data),
+                )
+                log_to_nn_state_dict(
+                    f"model.decoder.{self.layer_idx}.pp_block.attn.without_resid.after_layerscale",
+                    compute_stas(hidden_states),
+                )
+                log_to_nn_state_dict(
+                    f"model.decoder.{self.layer_idx}.pp_block.attn_and_resid_ratio.after_layerscale",
+                    {"value": hidden_states.norm(p=2) / residual.norm(p=2)},
+                )
+
         hidden_states = hidden_states + residual
 
         residual = hidden_states
@@ -844,6 +903,38 @@ class LlamaDecoderLayer(nn.Module):
         # hidden_states = F.layer_norm(hidden_states, self.normalized_shape, post_attention_ln_weight, self.post_attention_layernorm.bias, eps=self.post_attention_layernorm.eps)
         # NOTE: got nan starts from here in 1st layer
         hidden_states = self.mlp(hidden_states=hidden_states)["hidden_states"]
+
+        if constants.is_ready_to_log is True:
+            log_to_nn_state_dict(
+                f"model.decoder.{self.layer_idx}.pp_block.mlp.without_resid.before_layerscale",
+                compute_stas(hidden_states),
+            )
+            log_to_nn_state_dict(f"model.decoder.{self.layer_idx}.pp_block.mlp.resid", compute_stas(residual))
+            log_to_nn_state_dict(
+                f"model.decoder.{self.layer_idx}.pp_block.mlp_and_resid_ratio.before_layerscale",
+                {"value": hidden_states.norm(p=2) / residual.norm(p=2)},
+            )
+
+        from nanotron.constants import CONFIG
+
+        if CONFIG.fp8.layer_scale is True:
+            hidden_states = hidden_states * rearrange(self.mlp_layer_scale.data, "f -> 1 1 f")
+
+            if constants.is_ready_to_log is True:
+                # NN_STATES[f"model.decoder.{self.layer_idx}.pp_block.mlp.layerscale"] = {"absmax": self.mlp_layer_scale.abs().max()}
+                log_to_nn_state_dict(
+                    f"model.decoder.{self.layer_idx}.pp_block.mlp.layerscale",
+                    {"absmax": self.mlp_layer_scale.abs().max()},
+                )
+                log_to_nn_state_dict(
+                    f"model.decoder.{self.layer_idx}.pp_block.mlp.without_resid.after_layerscale",
+                    compute_stas(hidden_states),
+                )
+                log_to_nn_state_dict(
+                    f"model.decoder.{self.layer_idx}.pp_block.mlp_and_resid_ratio.after_layerscale",
+                    {"value": hidden_states.norm(p=2) / residual.norm(p=2)},
+                )
+
         hidden_states = hidden_states + residual
 
         return {

src/nanotron/scaling/monitor.py
@@ -8,6 +8,16 @@ from torch import nn
 from torch.distributed import ReduceOp
 
 
+def log_to_nn_state_dict(name, logs):
+    from nanotron import constants
+
+    if name not in constants.NN_STATES:
+        constants.NN_STATES[name] = {}
+
+    for key, value in logs.items():
+        constants.NN_STATES[name][key] = value
+
+
 def track_weight_and_grad_stats(name: str, module: nn.Module, parallel_context: ParallelContext):
     def compute_stats(tensors, metrics: List[str] = ["amax", "mean", "std", "var", "norm"]):
         from nanotron.fp8.utils import compute_stas
@@ -47,10 +57,10 @@ def track_weight_and_grad_stats(name: str, module: nn.Module, parallel_context: 
 
         return stats[list(stats.keys())[0]] if len(stats) == 1 else stats
 
-    logs: Dict[str, Dict[str, float]] = {}
+    # logs: Dict[str, Dict[str, float]] = {}
 
-    if name not in logs:
-        logs[name] = {}
+    # if name not in logs:
+    #     logs[name] = {}
 
     def _save_output_stats(module: nn.Module, input: torch.Tensor, output: torch.Tensor):
         param_names = [name for name, _ in module.named_parameters()]
@@ -59,7 +69,8 @@ def track_weight_and_grad_stats(name: str, module: nn.Module, parallel_context: 
                 param = getattr(module, param_name)
                 stats = compute_stats(param.data)
                 if stats is not None:
-                    logs[name][param_name] = stats
+                    # logs[name][param_name] = stats
+                    log_to_nn_state_dict(name, {param_name: stats})
 
         inputs = input if isinstance(input, tuple) else (input,)
         outputs = output if isinstance(output, tuple) else (output,)
@@ -71,20 +82,27 @@ def track_weight_and_grad_stats(name: str, module: nn.Module, parallel_context: 
                     continue
                 stats = compute_stats(inp)
                 if stats is not None:
-                    logs[name][f"input:{i}"] = stats
+                    # logs[name][f"input:{i}"] = stats
+                    log_to_nn_state_dict(name, {f"input:{i}": stats})
+
         elif len(inputs) == 1:
             stats = compute_stats(inputs[0])
             if stats is not None:
-                logs[name]["input"] = stats
+                # logs[name]["input"] = stats
+                log_to_nn_state_dict(name, {"input": stats})
+
         if len(outputs) > 1:
             for i, out in enumerate(outputs):
                 stats = compute_stats(out)
                 if stats is not None:
-                    logs[name][f"output:{i}"] = stats
+                    # logs[name][f"output:{i}"] = stats
+                    log_to_nn_state_dict(name, {f"output:{i}": stats})
+
         elif len(outputs) == 1:
             stats = compute_stats(outputs[0])
             if stats is not None:
-                logs[name]["output"] = stats
+                # logs[name]["output"] = stats
+                log_to_nn_state_dict(name, {"output": stats})
 
     def _save_grad_stats(module: nn.Linear, grad_input, grad_output: torch.Tensor):
         if isinstance(grad_output, tuple):
@@ -94,52 +112,78 @@ def track_weight_and_grad_stats(name: str, module: nn.Module, parallel_context: 
 
                 stats = compute_stats(grad)
                 if stats is not None:
-                    logs[name][f"grad_output:{i}"] = stats
+                    # logs[name][f"grad_output:{i}"] = stats
+                    log_to_nn_state_dict(name, {f"grad_output:{i}": stats})
         else:
             stats = compute_stats(grad_output)
             if stats is not None:
-                logs[name]["grad_output"] = stats
+                # logs[name]["grad_output"] = stats
+                log_to_nn_state_dict(name, {"grad_output": stats})
 
         if isinstance(grad_input, tuple):
             for i, grad in enumerate(grad_input):
                 if grad is not None:
                     stats = compute_stats(grad)
                     if stats is not None:
-                        logs[name][f"grad_input:{i}"] = stats
+                        # logs[name][f"grad_input:{i}"] = stats
+                        log_to_nn_state_dict(name, {f"grad_input:{i}": stats})
         else:
             if grad_input is not None:
                 stats = compute_stats(grad_input)
                 if stats is not None:
-                    logs[name]["grad_input"] = stats
+                    # logs[name]["grad_input"] = stats
+                    log_to_nn_state_dict(name, {"grad_input": stats})
 
     handles = []
     handles.append(module.register_forward_hook(_save_output_stats))
     # handles.append(module.register_backward_hook(_save_grad_stats))
-    return logs, handles
+    # return logs, handles
+    return handles
 
 
 def monitor_model(
     model: NanotronModel, parallel_context: ParallelContext
 ) -> Tuple[Dict[str, Union[torch.Tensor, float]], List]:
-    logs = {}
     handles = []
     leaf_modules = [(name, module) for name, module in model.named_modules()]
 
     for name, module in leaf_modules:
-        module_logs, module_handles = track_weight_and_grad_stats(name, module, parallel_context)
-        logs.update(module_logs)
+        # module_logs, module_handles = track_weight_and_grad_stats(name, module, parallel_context)
+        module_handles = track_weight_and_grad_stats(name, module, parallel_context)
+        # logs.update(module_logs)
         handles.extend(module_handles)
 
-    return logs, handles
+    # return logs, handles
+    return handles
+
+
+# def convert_logs_to_flat_logs(
+#     logs: Dict[str, Dict[str, Dict[str, Union[torch.Tensor, float]]]]
+# ) -> Dict[str, Union[torch.Tensor, float]]:
+#     flat_logs = {}
+#     for module_name, components in logs.items():
+#         if isinstance(components, dict) is True:
+#             for component_name, stats in components.items():
+#                 for metric_name, metric_value in stats.items():
+#                     flat_logs[f"{module_name}:{component_name}:{metric_name}"] = metric_value
+#         else:
+#             assert 1 == 1
+
+#     return flat_logs
 
 
 def convert_logs_to_flat_logs(
-    logs: Dict[str, Dict[str, Dict[str, Union[torch.Tensor, float]]]]
+    logs, prefix: str = "", flat_logs: Dict[str, Union[torch.Tensor, float]] = None
 ) -> Dict[str, Union[torch.Tensor, float]]:
-    flat_logs = {}
-    for module_name, components in logs.items():
-        for component_name, stats in components.items():
-            for metric_name, metric_value in stats.items():
-                flat_logs[f"{module_name}:{component_name}:{metric_name}"] = metric_value
+    if flat_logs is None:
+        flat_logs = {}
+
+    for key, value in logs.items():
+        new_prefix = f"{prefix}:{key}" if prefix else key
+
+        if isinstance(value, dict):
+            convert_logs_to_flat_logs(value, new_prefix, flat_logs)
+        else:
+            flat_logs[new_prefix] = value
 
     return flat_logs

src/nanotron/scaling/parametrization.py
@@ -4,8 +4,10 @@ from enum import Enum, auto
 from typing import Dict
 
 import torch
+from nanotron import logging
 from nanotron.config import ModelArgs
 from nanotron.fp8.tensor import FP8Tensor
+from nanotron.logging import log_rank
 from nanotron.nn.layer_norm import TritonRMSNorm
 from nanotron.parallel.parameters import get_data_from_param
 from nanotron.parallel.tensor_parallel.nn import (
@@ -17,6 +19,8 @@ from nanotron.parallel.tensor_parallel.nn import (
 )
 from torch import nn
 from torch.nn import init
+
+logger = logging.get_logger(__name__)
 
 
 class ParametrizationMethod(Enum):
@@ -30,7 +34,15 @@ class Parametrizator:
 
     def parametrize(self, param_name: str, module: nn.Module):
         if not isinstance(module, tuple(self.MODULE_TO_PARAMETRIZE.keys())):
-            raise Exception(f"Parameter {param_name} wasn't defined in the parametrization method.")
+            log_rank(
+                f"Skipping parametrization of {module.__class__.__name__}, param_name: {param_name}",
+                logger=logger,
+                level=logging.INFO,
+                rank=0,
+            )
+            return
+
+            # raise Exception(f"Parameter {param_name} wasn't defined in the parametrization method.")
 
         return self.MODULE_TO_PARAMETRIZE[type(module)](param_name, module)
 

src/nanotron/trainer.py
@@ -473,7 +473,12 @@ class DistributedTrainer:
             for self.iteration_step in range(self.metadata.last_train_step + 1, self.config.tokens.train_steps + 1):
                 from nanotron import constants
 
-                is_ready_to_log = (self.iteration_step - 1) % self.config.logging.iteration_step_info_interval == 0
+                is_ready_to_log = (
+                    ((self.iteration_step - 1) % self.config.logging.iteration_step_info_interval == 0)
+                    and (self.config.logging.monitor_model_states is True)
+                    and (dist.get_rank(self.parallel_context.world_pg) == 0)
+                )
+
                 constants.is_ready_to_log = is_ready_to_log
 
                 if isinstance(prof, torch.profiler.profile):
@@ -485,11 +490,14 @@ class DistributedTrainer:
                 self.iteration_start_time = time.time()
                 torch.cuda.empty_cache()
                 self._update_dataloader_based_on_training_stages(dataloader_or_dls)
-                if is_ready_to_log is True and self.config.logging.monitor_model_states is True:
+                if is_ready_to_log is True:
                     from nanotron.scaling.monitor import monitor_model
 
-                    nn_logs, state_handles = monitor_model(self.unwrapped_model, self.parallel_context)
-                    constants.NN_STATES = nn_logs
+                    if self.config.logging.monitor_model_states_using_hooks is True:
+                        # nn_logs, state_handles = monitor_model(self.unwrapped_model, self.parallel_context)
+                        state_handles = monitor_model(self.unwrapped_model, self.parallel_context)
+
+                    # constants.NN_STATES = nn_logs
 
                 # Training step
                 outputs, loss_avg = self.training_step(dataloader=self.current_dataloader)
@@ -510,8 +518,9 @@ class DistributedTrainer:
                         and dist.get_rank(self.parallel_context.world_pg) == 0
                         and wandb is not None
                     ):
-                        for handle in state_handles:
-                            handle.remove()
+                        if self.config.logging.monitor_model_states_using_hooks is True:
+                            for handle in state_handles:
+                                handle.remove()
 
                         detailed_logs = {}
 
@@ -520,7 +529,10 @@ class DistributedTrainer:
                             optim_logs = self.optimizer.optimizer.loggings
                             detailed_logs.update(optim_logs)
 
-                        detailed_logs.update(convert_logs_to_flat_logs(nn_logs))
+                        from nanotron import constants
+
+                        # detailed_logs.update(convert_logs_to_flat_logs(nn_logs))
+                        detailed_logs.update(convert_logs_to_flat_logs(constants.NN_STATES))
                         # NOTE: convert tensor to float for logging
                         detailed_logs = {
                             k: v.item() if isinstance(v, torch.Tensor) else v for k, v in detailed_logs.items()
@@ -528,17 +540,16 @@ class DistributedTrainer:
 
                         # NOTE: save the detailed logs, path/{run_name}/{iteration_step}/logs.json
 
-                        from nanotron.constants import get_debug_save_path
 
                         # DEBUG_SAVE_PATH = DEBUG_SAVE_PATH.format(self.config.general.run, self.iteration_step)
 
-                        debug_save_path = get_debug_save_path(self.config.general.run, self.iteration_step)
+                        # debug_save_path = get_debug_save_path(self.config.general.run, self.iteration_step)
 
-                        with open(f"{debug_save_path}/logs.json", "w") as f:
-                            json.dump(detailed_logs, f)
+                        # with open(f"{debug_save_path}/logs.json", "w") as f:
+                        #     json.dump(detailed_logs, f)
 
-                        constants.NN_STATES = None
                         wandb.log({**detailed_logs, "iteration_step": self.iteration_step})
+                        constants.NN_STATES = {}
 
                 # Checkpoint
                 if self.iteration_step % self.config.checkpoints.checkpoint_interval == 0: