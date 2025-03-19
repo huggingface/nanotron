0c8f74b0320d7f6d36d11299d8434c34305048ea
nouamanetazi <nouamane98@gmail.com>
2025-03-19 03:31:16 +0000

    .

--- src/nanotron/trainer.py
+++ src/nanotron/trainer.py
@@ -58,6 +58,7 @@ from nanotron.logging import (
 from nanotron.models import NanotronModel, build_model
 from nanotron.models.base import check_model_has_grad
 from nanotron.models.llama import LlamaForTraining, RotaryEmbedding
+from nanotron.models.qwen import Qwen2ForTraining
 from nanotron.models.starcoder2 import Starcoder2ForTraining
 from nanotron.optim.clip_grads import clip_grad_norm
 from nanotron.parallel import ParallelContext
@@ -105,6 +106,7 @@ dist_logger.setLevel(logging.WARNING)
 CONFIG_TO_MODEL_CLASS = {
     "LlamaConfig": LlamaForTraining,
     "Starcoder2Config": Starcoder2ForTraining,
+    "Qwen2Config": Qwen2ForTraining,
 }
 
 try:
