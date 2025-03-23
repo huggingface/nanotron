5c19ae5be37a49b6dc347c126d6c5b09e751723f
nouamanetazi <nouamane98@gmail.com>
2025-03-23 07:32:40 +0000

    .

--- src/nanotron/logging.py
+++ src/nanotron/logging.py
@@ -415,6 +415,7 @@ def set_ranks_logging_level(parallel_context: ParallelContext, logging_config: "
 def log_libraries_versions(logger: logging.Logger):
     import datasets
     import flash_attn
+    import numpy
     import torch
     import transformers
 
@@ -426,6 +427,8 @@ def log_libraries_versions(logger: logging.Logger):
     log_rank(f"transformers version: {transformers.__version__}", logger=logger, level=logging.INFO, rank=0)
     log_rank(f"datasets version: {datasets.__version__}", logger=logger, level=logging.INFO, rank=0)
     log_rank(f"flash-attn version: {flash_attn.__version__}", logger=logger, level=logging.INFO, rank=0)
+    log_rank(f"numpy version: {numpy.__version__}", logger=logger, level=logging.INFO, rank=0)
+    log_rank(f"\ntorch.utils.collect_env: {torch.utils.collect_env.main()}", logger=logger, level=logging.INFO, rank=0)
 
 
 _configure_library_root_logger()
