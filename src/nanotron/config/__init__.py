c671db965e440be9f92f1afcf73d244ec50d8e7e
“eliebak” <elie.bakouch@huggingface.co>
2025-03-26 01:41:19 +0000

    clean

--- src/nanotron/config/__init__.py
+++ src/nanotron/config/__init__.py
@@ -1,27 +1,5 @@
 # flake8: noqa
-from nanotron.config.config import (
-    AdamWOptimizerArgs,
-    BenchArgs,
-    CheckpointsArgs,
-    Config,
-    DataArgs,
-    DatasetStageArgs,
-    GeneralArgs,
-    LRSchedulerArgs,
-    LoggingArgs,
-    ModelArgs,
-    NanosetDatasetsArgs,
-    OptimizerArgs,
-    ParallelismArgs,
-    PretrainDatasetsArgs,
-    ProfilerArgs,
-    SGDOptimizerArgs,
-    TokenizerArgs,
-    TokensArgs,
-    ZLossArgs,
-    get_config_from_dict,
-    get_config_from_file,
-)
+from nanotron.config.config import *
 from nanotron.config.models_config import *
 from nanotron.config.utils_config import *
 from nanotron.config.lighteval_config import *
