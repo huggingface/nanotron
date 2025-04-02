f4607e092e05fbe5f0a009c5bc785ef0565cbe0a
elie <97572401+eliebak@users.noreply.github.com>
2025-04-02 22:54:09 +0200

    Nanoset clean up old index (#335)
    * .
    
    * .
    
    * tokenizer eos
    
    * .
    
    * forgot the assert

--- run_train.py
+++ run_train.py
@@ -185,8 +185,15 @@ def get_dataloader_from_data_stage(
         from nanotron.data.nanoset import Nanoset
 
         with main_rank_first(trainer.parallel_context.world_pg):
-            logger.info(
-                f"[Nanoset] Creating Nanoset with {len(data.dataset.dataset_folder)} dataset folders and {trainer.config.tokens.train_steps * trainer.global_batch_size} train samples"
+            tokenizer_path = trainer.config.tokenizer.tokenizer_name_or_path
+            tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+            eos_token_id = tokenizer.eos_token_id
+            assert eos_token_id is not None and data.dataset.return_positions is True, "Tokenizer must have an eos token if return_positions is True"
+            log_rank(
+                f"[Nanoset] Creating Nanoset with {len(data.dataset.dataset_folder)} dataset folders and {trainer.config.tokens.train_steps * trainer.global_batch_size} train samples",
+                logger=logger,
+                level=logging.INFO,
+                rank=0,
             )
             start_time = time.time()
             train_dataset = Nanoset(
@@ -196,10 +203,15 @@ def get_dataloader_from_data_stage(
                 train_split_num_samples=trainer.config.tokens.train_steps * trainer.global_batch_size,
                 dataset_weights=data.dataset.dataset_weights,
                 random_seed=data.seed,
+                return_positions=data.dataset.return_positions,
+                eos_token_id=eos_token_id,
             )
             end_time = time.time()
-            logger.info(
-                f"[Nanoset] Time taken to create Nanoset: {time.strftime('%M:%S', time.gmtime(end_time - start_time))} (MM:SS)"
+            log_rank(
+                f"[Nanoset] Time taken to create Nanoset: {time.strftime('%M:%S', time.gmtime(end_time - start_time))} (MM:SS)",
+                logger=logger,
+                level=logging.INFO,
+                rank=0,
             )
         # Prepare dataloader
         train_dataloader = build_nanoset_dataloader(
