35fb92faac4432138dc63405dfde4a2eaf3ad0da
Langwen Huang <huanglangwen@outlook.com>
2025-11-12 21:38:06 +0100

    update

--- climllama/build_venv.sh
+++ climllama/build_venv.sh
@@ -10,8 +10,13 @@ uv pip install torch --index-url https://download.pytorch.org/whl/cu129
 CURRENT_PATH=$(dirname "$BASH_SOURCE[0]")
 uv pip install -e $CURRENT_PATH/..
 
-uv pip install datasets transformers datatrove[io] numba wandb setuptools psutil ninja triton
+uv pip install datasets transformers datatrove[io] numba wandb setuptools psutil ninja triton pybind11
+
 # Fused kernels
 MAX_JOBS=8 uv pip install "flash-attn>=2.5.0" --no-build-isolation
 MAX_JOBS=16 uv pip install --no-build-isolation grouped-gemm
 MAX_JOBS=16 uv pip install --no-build-isolation transformer_engine[pytorch]
+
+CURRENT_DIR=$(pwd)
+cd src/nanotron/data/nemo_dataset && make
+cd $CURRENT_DIR
