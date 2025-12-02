7593adf9494747eee89270dfe3279eb675c670b5
Langwen Huang <huanglangwen@outlook.com>
2025-12-02 13:57:30 +0100

    add run failed test

---
+++ climllama/run_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Run failed ClimLlama tests
+
+pytest tests/test_climllama.py::test_spatial_temporal_projection_dimensions[2-1-2] \
+       tests/test_climllama.py::test_different_position_ids_per_batch[2-2-1] \
+       tests/test_climllama.py::test_embedding_without_discrete_positions[2-2-1] \
+       tests/test_climllama.py::test_embedding_forward_backward[4-1-1] \
+       tests/test_climllama.py::test_embedding_combines_all_components[2-1-2] \
+       tests/test_climllama.py::test_embedding_output_shape[2-2-1] \
+       tests/test_climllama.py::test_embedding_combines_all_components[4-1-1] \
+       tests/test_climllama.py::test_position_ids_passed_through[2-1-2] \
+       tests/test_climllama.py::test_spatial_temporal_projection_dimensions[2-2-1] \
+       tests/test_climllama.py::test_spatial_temporal_projection_dimensions[4-1-1] \
+       tests/test_climllama.py::test_embedding_forward_backward[2-1-2] \
+       tests/test_climllama.py::test_embedding_without_discrete_positions[4-1-1] \
+       tests/test_climllama.py::test_embedding_forward_backward[2-2-1] \
+       tests/test_climllama.py::test_embedding_without_discrete_positions[2-1-2] \
+       tests/test_climllama.py::test_position_ids_passed_through[4-1-1] \
+       tests/test_climllama.py::test_embedding_output_shape[4-1-1] \
+       tests/test_climllama.py::test_embedding_combines_all_components[2-2-1] \
+       tests/test_climllama.py::test_different_position_ids_per_batch[2-1-2] \
+       tests/test_climllama.py::test_position_ids_passed_through[2-2-1] \
+       tests/test_climllama.py::test_different_position_ids_per_batch[4-1-1] \
+       tests/test_climllama.py::test_collator_output_structure[2-1-2] \
+       tests/test_climllama.py::test_embedding_output_shape[2-1-2] \
+       tests/test_climllama.py::test_collator_output_structure[1-2-2] \
+       tests/test_climllama.py::test_collator_position_ids_shape[4-1-1] \
+       tests/test_climllama.py::test_collator_without_doc_masking[1-2-2] \
+       tests/test_climllama.py::test_collator_label_mask_with_doc_boundaries[2-1-2] \
+       tests/test_climllama.py::test_collator_non_participating_rank[1-2-2] \
+       tests/test_climllama.py::test_collator_non_participating_rank[2-1-2] \
+       tests/test_climllama.py::test_collator_position_ids_shape[1-2-2] \
+       tests/test_climllama.py::test_collator_label_mask_with_doc_boundaries[1-2-2] \
+       tests/test_climllama.py::test_collator_position_ids_shape[2-2-1] \
+       tests/test_climllama.py::test_collator_position_ids_shape[2-1-2] \
+       tests/test_climllama.py::test_collator_without_doc_masking[2-1-2] \
+       -v
