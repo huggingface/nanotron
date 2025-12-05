8fbf4aa8f1a3ee5e91156fda263677d099daa409
Langwen Huang <huanglangwen@outlook.com>
2025-12-05 21:54:30 +0100

    update

--- climllama/run_finetune_debug.sh
+++ climllama/run_finetune_debug.sh
@@ -3,7 +3,7 @@
 CONFIG_FILE=/capstor/scratch/cscs/lhuang/nanotron_climllama/climllama/config_finetune_with_pe_climllama_test.yaml
 WORKDIR=/capstor/scratch/cscs/lhuang/nanotron_climllama
 
-ipython -i -m torch.distributed.run -- \
+ipython -i --pdb -m torch.distributed.run -- \
  --nproc_per_node=1 \
  --node_rank 0 \
  --nnodes=1 \
