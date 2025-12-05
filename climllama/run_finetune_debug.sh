35f155550651772fdaf8dbd8e4e7329fb4a0752b
Langwen Huang <huanglangwen@outlook.com>
2025-12-05 23:09:31 +0100

    Update debug

--- climllama/run_finetune_debug.sh
+++ climllama/run_finetune_debug.sh
@@ -2,9 +2,15 @@
 
 CONFIG_FILE=/capstor/scratch/cscs/lhuang/nanotron_climllama/climllama/config_finetune_with_pe_climllama_test.yaml
 WORKDIR=/capstor/scratch/cscs/lhuang/nanotron_climllama
-export WORLD_SIZE=1
-export RANK=0
-export MASTER_ADDR=localhost
-export MASTER_PORT=18899
-export LOCAL_RANK=0
-ipython -i --pdb $WORKDIR/run_train.py -- --config-file $CONFIG_FILE
+#export WORLD_SIZE=1
+#export RANK=0
+#export MASTER_ADDR=localhost
+#export MASTER_PORT=18899
+#export LOCAL_RANK=0
+#ipython -i --pdb $WORKDIR/run_train.py -- --config-file $CONFIG_FILE
+
+torchrun --nproc_per_node=4 \
+ --node_rank 0 \
+ --nnodes=1 \
+ --start-method forkserver \
+ $WORKDIR/run_train.py --config-file $CONFIG_FILE
