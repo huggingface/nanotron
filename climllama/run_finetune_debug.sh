6eab633436f1d99787e68e43fdf73570bd04fe40
Langwen Huang <langwen.huang@inf.ethz.ch>
2025-12-05 22:10:55 +0100

    update debug cmd:

--- climllama/run_finetune_debug.sh
+++ climllama/run_finetune_debug.sh
@@ -2,10 +2,9 @@
 
 CONFIG_FILE=/capstor/scratch/cscs/lhuang/nanotron_climllama/climllama/config_finetune_with_pe_climllama_test.yaml
 WORKDIR=/capstor/scratch/cscs/lhuang/nanotron_climllama
-
-ipython -i --pdb -m torch.distributed.run -- \
- --nproc_per_node=1 \
- --node_rank 0 \
- --nnodes=1 \
- --start-method forkserver \
- $WORKDIR/run_train.py --config-file $CONFIG_FILE
+export WORLD_SIZE=1
+export RANK=0
+export MASTER_ADDR=localhost
+export MASTER_PORT=18899
+export LOCAL_RANK=0
+ipython -i --pdb $WORKDIR/run_train.py -- --config-file $CONFIG_FILE
