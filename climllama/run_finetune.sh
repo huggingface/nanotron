7208b5c056a9f9259a903b10173ead0711787625
Langwen Huang <huanglangwen@outlook.com>
2025-12-07 20:59:46 +0100

    update num nodes according to dp

--- climllama/run_finetune.sh
+++ climllama/run_finetune.sh
@@ -2,8 +2,8 @@
 
 CONFIG_FILE_106K=/capstor/scratch/cscs/lhuang/nanotron_climllama/climllama/configs/config_finetune_forecast_106k.yaml
 export CONFIG_FILE=${CONFIG_FILE_106K}
-sbatch -J finetune_climllama_forecast_106k run_finetune.sbatch
+sbatch -N 48 -J finetune_climllama_forecast_106k run_finetune.sbatch
 
 CONFIG_FILE_52K=/capstor/scratch/cscs/lhuang/nanotron_climllama/climllama/configs/config_finetune_forecast_52k.yaml
 export CONFIG_FILE=${CONFIG_FILE_52K}
-sbatch -J finetune_climllama_forecast_52k run_finetune.sbatch
+sbatch -N 36 -J finetune_climllama_forecast_52k run_finetune.sbatch
