ee6f59ac6673e202d5649b8cf31f9430b07affb9
Langwen Huang <huanglangwen@outlook.com>
2025-12-08 13:01:14 +0100

    Continue training

--- climllama/run_finetune.sh
+++ climllama/run_finetune.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-CONFIG_FILE_106K=/capstor/scratch/cscs/lhuang/nanotron_climllama/climllama/configs/config_finetune_forecast_106k.yaml
+#CONFIG_FILE_106K=/capstor/scratch/cscs/lhuang/nanotron_climllama/climllama/configs/config_finetune_forecast_106k.yaml
+CONFIG_FILE_106K=/capstor/scratch/cscs/lhuang/nanotron_climllama/climllama/configs/config_finetune_forecast_106k_continue.yaml
 export CONFIG_FILE=${CONFIG_FILE_106K}
 sbatch -N 48 -J finetune_climllama_forecast_106k run_finetune.sbatch
 
-CONFIG_FILE_52K=/capstor/scratch/cscs/lhuang/nanotron_climllama/climllama/configs/config_finetune_forecast_52k.yaml
-export CONFIG_FILE=${CONFIG_FILE_52K}
-sbatch -N 36 -J finetune_climllama_forecast_52k run_finetune.sbatch
+#CONFIG_FILE_52K=/capstor/scratch/cscs/lhuang/nanotron_climllama/climllama/configs/config_finetune_forecast_52k.yaml
+#export CONFIG_FILE=${CONFIG_FILE_52K}
+#sbatch -N 36 -J finetune_climllama_forecast_52k run_finetune.sbatch
