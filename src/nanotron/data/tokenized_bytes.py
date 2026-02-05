6ceb82c3ad88bd397a21312bbdf8692a4bea4ca7
Ale <ale.ercolani@gmail.com>
2026-02-05 13:03:21 +0100

    the parameters passing to the super class were old and wrong

--- src/nanotron/data/tokenized_bytes.py
+++ src/nanotron/data/tokenized_bytes.py
@@ -423,14 +423,15 @@ class TokenizedBytesFolderDataset(DatatroveFolderDataset):
             filename_pattern=filename_pattern,
             recursive=recursive,
             token_size=token_size,
-            max_tokens=max_tokens,
+            #max_tokens=max_tokens,
             shuffle=shuffle,
             seed=seed,
             return_positions=return_positions,
-            eos_token_id=eos_token_id,
-            read_path=folder_read_path,
-            matched_files=matched_files,
-            file_sizes=file_sizes,
+            positions_from_eos_token_id=eos_token_id,
+            #eos_token_id=eos_token_id,
+            #read_path=folder_read_path,
+            #matched_files=matched_files,
+            #file_sizes=file_sizes,
         )
 
         self.subset_log = TBFolderDatasetLog(
